ed44cff23222aa942f7938a15cac51704562c62a
planet36 <planet36@users.noreply.github.com>
2021-01-21 10:40:37 -0500

    Add wrapper for boincmgr

---
+++ link/.config/fish/functions/boincmgr.fish
@@ -0,0 +1,8 @@
+
+function boincmgr --description 'boincmgr with datadir specified' --wraps boincmgr
+
+	mkdir --verbose -- /media/sf_shared/boinc-data || return
+
+	command boincmgr --datadir=/media/sf_shared/boinc-data
+end
+
