2e4a418427b9847555459b6eaaa577686e2bf272
planet36 <planet36@users.noreply.github.com>
2021-02-10 18:08:39 -0500

    Add cproto.fish

---
+++ link/.config/fish/functions/cproto.fish
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+function cproto --wraps ctags --description 'Generate function prototypes for the given C file(s)'
+
+    if test (count $argv) -eq 0
+        return 1
+    end
+
+    ctags --output-format=json --totals=no --extras=-F --fields=nP $argv | jq -r --slurp 'sort_by(.line) | .[].pattern | ltrimstr("/^") | rtrimstr("$/") | . + ";"'
+end
