75c2fdb815e7187872584ee6957fbc1bee57d94c
planet36 <planet36@users.noreply.github.com>
2021-03-18 18:41:29 -0400

    Rename TGT_DIR to PATCHES_DIR
    Don't compress the tar file or move it

--- link/.config/fish/functions/tfp.fish
+++ link/.config/fish/functions/tfp.fish
@@ -3,21 +3,19 @@
 
 function tfp --description 'tar format patch'
 
-    set TGT_DIR (basename (git rev-parse --show-toplevel)) || return
+    set PATCHES_DIR (basename (git rev-parse --show-toplevel)) || return
 
     # XXX: string replace returns 1 (failure) if no replacement was performed
-    set TGT_DIR patches-(string replace --regex --all '\W' '' $TGT_DIR)
+    set PATCHES_DIR patches-(string replace --regex --all '\W' '' $PATCHES_DIR)
 
-    mkdir --verbose -- "$TGT_DIR" || return
+    mkdir --verbose -- "$PATCHES_DIR" || return
 
-    git format-patch -o "$TGT_DIR" --keep-subject --base=auto --progress (git rev-parse --abbrev-ref HEAD@{upstream})..HEAD || return
+    git format-patch -o "$PATCHES_DIR" --keep-subject --base=auto --progress (git rev-parse --abbrev-ref HEAD@{upstream})..HEAD || return
 
-    if dir_is_empty "$TGT_DIR"
-        rmdir --verbose -- "$TGT_DIR" || return
+    if dir_is_empty "$PATCHES_DIR"
+        rmdir --verbose -- "$PATCHES_DIR" || return
     else
-        t "$TGT_DIR" && xz -9 -e "$TGT_DIR".tar && del "$TGT_DIR" || return
-        mv --verbose -- "$TGT_DIR".tar.xz ~/Downloads || return
-        printf 'del %s\n' ~/Downloads/"$TGT_DIR".tar.xz
+        t "$PATCHES_DIR" && del "$PATCHES_DIR" || return
     end
 end
 
