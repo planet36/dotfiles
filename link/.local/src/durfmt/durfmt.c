0781ee5fa0e9290c118b3c4fc91cf44fc7f60085
planet36 <planet36@users.noreply.github.com>
2021-04-15 18:23:02 -0400

    Add durfmt program

---
+++ link/.local/src/durfmt/durfmt.c
@@ -0,0 +1,175 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+const char program_author[] = "Steven Ward";
+const char program_version[] = "21w15a"; // date +'%yw%Ua'
+
+void my_div_i(
+		const unsigned long x, const unsigned long y,
+		unsigned long* quo, unsigned long* rem)
+{
+	*quo = (x / y);
+	*rem = (x % y);
+}
+
+const unsigned long seconds_per_minute = 60U;
+const unsigned long seconds_per_hour = seconds_per_minute * 60U;
+const unsigned long seconds_per_day = seconds_per_hour * 24U;
+const unsigned long seconds_per_week = seconds_per_day * 7U;
+const unsigned long seconds_per_year = 31556952U; // seconds_per_day * 365.2425
+
+void durfmt(unsigned long seconds, const bool print_zero_seconds)
+{
+	bool printed_something = false;
+	unsigned long years = 0;
+	unsigned long weeks = 0;
+	unsigned long days = 0;
+	unsigned long hours = 0;
+	unsigned long minutes = 0;
+
+	my_div_i(seconds, seconds_per_year, &years, &seconds);
+	my_div_i(seconds, seconds_per_week, &weeks, &seconds);
+	my_div_i(seconds, seconds_per_day, &days, &seconds);
+	my_div_i(seconds, seconds_per_hour, &hours, &seconds);
+	my_div_i(seconds, seconds_per_minute, &minutes, &seconds);
+
+	if (years > 0)
+	{
+		if (printed_something)
+			putchar(' ');
+		printf("%luy", years);
+		printed_something = true;
+	}
+
+	if (weeks > 0)
+	{
+		if (printed_something)
+			putchar(' ');
+		printf("%luw", weeks);
+		printed_something = true;
+	}
+
+	if (days > 0)
+	{
+		if (printed_something)
+			putchar(' ');
+		printf("%lud", days);
+		printed_something = true;
+	}
+
+	if (hours > 0)
+	{
+		if (printed_something)
+			putchar(' ');
+		printf("%luh", hours);
+		printed_something = true;
+	}
+
+	if (minutes > 0)
+	{
+		if (printed_something)
+			putchar(' ');
+		printf("%lum", minutes);
+		printed_something = true;
+	}
+
+	if (seconds > 0 || (print_zero_seconds && !printed_something))
+	{
+		if (printed_something)
+			putchar(' ');
+		printf("%lus", seconds);
+		printed_something = true;
+	}
+
+	if (printed_something)
+		putchar('\n');
+}
+
+void print_version(const char* argv0)
+{
+	printf("%s %s\n", argv0, program_version);
+	printf("Written by %s\n", program_author);
+}
+
+void print_usage(const char* argv0)
+{
+	printf("Usage: %s [OPTIONS]\n\n", argv0);
+	printf("Read a non-negative duration (in seconds) from stdin.\n");
+	printf("Convert the value to a human-friendly format of whole numbers of:\n");
+	printf("    years, weeks, days, hours, minutes, and seconds.\n\n");
+	printf("OPTIONS\n");
+	printf("  -V    Print the version information and exit.\n");
+	printf("  -h    Print this message and exit.\n");
+	printf("  -0    Print zero seconds if nothing else was printed.\n");
+	printf("\n");
+
+}
+
+int main(int argc, char* argv[])
+{
+	bool print_zero_seconds = false;
+	const char* short_options = "+:Vh0";
+	int oc;
+
+	opterr = 0;
+	while ((oc = getopt(argc, argv, short_options)) != -1)
+	{
+		switch (oc)
+		{
+		case 'V':
+			print_version(argv[0]);
+			return 0;
+
+		case 'h':
+			print_usage(argv[0]);
+			return 0;
+
+		case '0':
+			print_zero_seconds = true;
+			break;
+
+		case '?':
+			if (isprint(optopt))
+				fprintf(stderr, "%s: Unknown option: '%c'\n", argv[0], optopt);
+			else
+				fprintf(stderr, "%s: Unknown option: '\\x%x'\n", argv[0], optopt);
+
+			return 1;
+
+		case ':':
+			if (isprint(optopt))
+				fprintf(stderr, "%s: Option requires a value: '%c'\n", argv[0], optopt);
+			else
+				fprintf(stderr, "%s: Option requires a value: '\\x%x'\n", argv[0], optopt);
+
+			return 1;
+
+		default:
+			return 1;
+		}
+	}
+
+	//argc -= optind;
+	//argv += optind;
+
+	char* line = NULL;
+	size_t n = 0;
+	//ssize_t bytes_read = 0;
+	//while ((bytes_read = getline(&line, &n, stdin)) != EOF)
+	while (getline(&line, &n, stdin) != EOF)
+	{
+		unsigned long seconds = strtoul(line, NULL, 0);
+		durfmt(seconds, print_zero_seconds);
+		free(line);
+		line = NULL;
+		n = 0;
+	}
+
+	return 0;
+}
