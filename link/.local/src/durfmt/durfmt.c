20b4b8e8266ee136f91f1de11fee7722407a51ce
planet36 <planet36@users.noreply.github.com>
2021-04-20 08:37:36 -0400

    Add UT_MAX enum value
    Order the durfmt_opts fields so the least significant units are first
    Update version

--- link/.local/src/durfmt/durfmt.c
+++ link/.local/src/durfmt/durfmt.c
@@ -9,7 +9,7 @@
 #include <unistd.h>
 
 const char program_author[] = "Steven Ward";
-const char program_version[] = "21w15f"; // date +'%yw%Ua'
+const char program_version[] = "21w16a"; // date +'%yw%Ua'
 
 /// unit of time
 enum UT
@@ -20,6 +20,7 @@ enum UT
 	UT_DAY,
 	UT_WEEK,
 	UT_YEAR,
+	UT_MAX, // must be last
 };
 
 int strtoi(const char* s)
@@ -48,64 +49,64 @@ const unsigned long seconds_per_year = 31556952UL; // seconds_per_day * 365.2425
 
 struct durfmt_opts
 {
-	int w_y;
-	int w_w;
-	int w_d;
-	int w_h;
-	int w_m;
 	int w_s;
+	int w_m;
+	int w_h;
+	int w_d;
+	int w_w;
+	int w_y;
 
-	char u_y;
-	char u_w;
-	char u_d;
-	char u_h;
-	char u_m;
 	char u_s;
+	char u_m;
+	char u_h;
+	char u_d;
+	char u_w;
+	char u_y;
 
-	bool p_y;
-	bool p_w;
-	bool p_d;
-	bool p_h;
-	bool p_m;
 	bool p_s;
+	bool p_m;
+	bool p_h;
+	bool p_d;
+	bool p_w;
+	bool p_y;
 
-	bool p_0y;
-	bool p_0w;
-	bool p_0d;
-	bool p_0h;
-	bool p_0m;
 	bool p_0s;
+	bool p_0m;
+	bool p_0h;
+	bool p_0d;
+	bool p_0w;
+	bool p_0y;
 };
 
 void durfmt_opts_init(struct durfmt_opts* opts)
 {
-	opts->w_y = 1;
-	opts->w_w = 1;
-	opts->w_d = 1;
-	opts->w_h = 1;
-	opts->w_m = 1;
 	opts->w_s = 1;
+	opts->w_m = 1;
+	opts->w_h = 1;
+	opts->w_d = 1;
+	opts->w_w = 1;
+	opts->w_y = 1;
 
-	opts->u_y = 'y';
-	opts->u_w = 'w';
-	opts->u_d = 'd';
-	opts->u_h = 'h';
-	opts->u_m = 'm';
 	opts->u_s = 's';
+	opts->u_m = 'm';
+	opts->u_h = 'h';
+	opts->u_d = 'd';
+	opts->u_w = 'w';
+	opts->u_y = 'y';
 
-	opts->p_y = true;
-	opts->p_w = true;
-	opts->p_d = true;
-	opts->p_h = true;
-	opts->p_m = true;
 	opts->p_s = true;
+	opts->p_m = true;
+	opts->p_h = true;
+	opts->p_d = true;
+	opts->p_w = true;
+	opts->p_y = true;
 
-	opts->p_0y = false;
-	opts->p_0w = false;
-	opts->p_0d = false;
-	opts->p_0h = false;
-	opts->p_0m = false;
 	opts->p_0s = false;
+	opts->p_0m = false;
+	opts->p_0h = false;
+	opts->p_0d = false;
+	opts->p_0w = false;
+	opts->p_0y = false;
 }
 
 void durfmt(
