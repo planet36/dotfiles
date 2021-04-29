d1b86b3bd0fd0f9fe8875f734c1dad8b16678cf9
planet36 <planet36@users.noreply.github.com>
2021-04-29 16:51:33 -0400

    Move utility functions to their own file

--- link/.local/src/as_bool/as_bool.c
+++ link/.local/src/as_bool/as_bool.c
@@ -1,63 +1,10 @@
 // SPDX-FileCopyrightText: Steven Ward
 // SPDX-License-Identifier: OSL-3.0
 
-/*
-time $CC $CFLAGS -Wno-cast-qual as_bool.c -o as_bool-c ; d as_bool-c
-*/
+#include "util.h"
 
-#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-
-/*
- * Copied from
- * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/lib/string.c#n516
- */
-
-/**
- * skip_spaces - Removes leading whitespace from @str.
- * @str: The string to be stripped.
- *
- * Returns a pointer to the first non-whitespace character in @str.
- */
-char *skip_spaces(const char *str)
-{
-	while (isspace(*str))
-		++str;
-	return (char *)str;
-}
-
-/**
- * strim - Removes leading and trailing whitespace from @s.
- * @s: The string to be stripped.
- *
- * Note that the first trailing whitespace is replaced with a %NUL-terminator
- * in the given string @s. Returns a pointer to the first non-whitespace
- * character in @s.
- */
-char *strim(char *s)
-{
-	size_t size;
-	char *end;
-
-	size = strlen(s);
-	if (!size)
-		return s;
-
-	end = s + size - 1;
-	while (end >= s && isspace(*end))
-		end--;
-	*(end + 1) = '\0';
-
-	return skip_spaces(s);
-}
-
-
-void strtolower(char* s)
-{
-	for (; *s; ++s) { *s = tolower(*s); }
-}
-
 
 // Evaluate the given string as a boolean
 int main(int argc, char* argv[])
---
+++ link/.local/src/as_bool/util.h
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+#pragma once
+
+char *skip_spaces(const char *str);
+char *strim(char *s);
+void strtolower(char* s);
