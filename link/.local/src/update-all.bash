09ba974ed328a8343ff1e7dfe03b9ee8ee67fe79
planet36 <planet36@users.noreply.github.com>
2021-05-27 00:08:32 -0400

    Only cd if it's a git repo

--- link/.local/src/update-all.bash
+++ link/.local/src/update-all.bash
@@ -5,17 +5,17 @@
 while IFS= read -r -d '' DIR
 do
   echo "## $DIR"
-  cd "$DIR" || break
-  if test -d .git
+  if test -d "$DIR"/.git
   then
+    cd "$DIR" || break
     git fetch || break
     git pull || break
     git fsck || break
     git gc || break
+    cd - &> /dev/null || break
   else
     echo "Not a git repo"
   fi
-  cd - &> /dev/null || break
   echo
   echo
 done < <(find . -type d -name .git -printf '%h\0' | sort --version-sort --zero-terminated)
--- link/.local/src/update-all.fish
+++ link/.local/src/update-all.fish
@@ -2,16 +2,16 @@
 
 for DIR in (find . -type d -name .git -printf '%h\n' | sort --version-sort)
   echo "## $DIR"
-  cd "$DIR" || break
-  if test -d .git
+  if test -d "$DIR"/.git
+    cd "$DIR" || break
     git fetch || break
     git pull || break
     git fsck || break
     git gc || break
+    cd - || break
   else
     echo "Not a git repo"
   end
-  cd - || break
   echo
   echo
 end
