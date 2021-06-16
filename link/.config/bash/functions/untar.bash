0ce240b0ae0c755f53d3c43a0af6706a0cacfe91
planet36 <planet36@users.noreply.github.com>
2021-06-16 10:52:56 -0400

    Remove unnecessary braces in variable expansion

--- link/.config/bash/functions/untar.bash
+++ link/.config/bash/functions/untar.bash
@@ -11,8 +11,8 @@ function untar
     renice --priority 19 --pid $BASHPID > /dev/null
 
     declare -r SCRIPT_NAME="${FUNCNAME[0]}"
-
     declare -r SCRIPT_VERSION='2019-09-19'
+    declare -r SCRIPT_AUTHOR='Steven Ward'
 
     VERBOSE=false
 
@@ -24,15 +24,15 @@ function untar
     function print_version
     {
         cat <<EOT
-${SCRIPT_NAME} ${SCRIPT_VERSION}
-Written by Steven Ward
+$SCRIPT_NAME $SCRIPT_VERSION
+Written by $SCRIPT_AUTHOR
 EOT
     }
 
     function print_help
     {
         cat <<EOT
-Usage: ${SCRIPT_NAME} [OPTIONS] FILE...
+Usage: $SCRIPT_NAME [OPTIONS] FILE...
 
 Untar FILE.
 
@@ -53,29 +53,28 @@ EOT
     function print_warning
     {
         printf 'Warning: ' 1>&2
-        printf -- "${@}" 1>&2
+        printf -- "$@" 1>&2
         printf '\n' 1>&2
     }
 
     function print_error
     {
         printf 'Error: ' 1>&2
-        printf -- "${@}" 1>&2
+        printf -- "$@" 1>&2
         printf '\n' 1>&2
 
-        printf 'Try "%q -h" for more information.\n' "${SCRIPT_NAME}" 1>&2
+        printf 'Try "%q -h" for more information.\n' "$SCRIPT_NAME" 1>&2
         exit 1
     }
 
     function print_verbose
     {
-        ${VERBOSE} && { printf -- "${@}" ; printf '\n' ; }
+        $VERBOSE && { printf -- "$@" ; printf '\n' ; }
     }
-
 
     while getopts "Vhvkt:" option
     do
-        case "${option}" in
+        case "$OPTION" in
 
             V) # version
                 print_version
@@ -96,14 +95,14 @@ EOT
             ;;
 
             t) # tar option
-                #echo "OPTARG: ${OPTARG}"
-                TAR_OPTIONS+=("${OPTARG}")
+                #echo "OPTARG: $OPTARG"
+                TAR_OPTIONS+=("$OPTARG")
                 #echo "TAR_OPTIONS=(${TAR_OPTIONS[@]})"
                 #printf -- "TAR_OPTIONS=(${TAR_OPTIONS[@]})\n"
             ;;
 
             *)
-                # Note: ${option} is '?'
+                # Note: $OPTION is '?'
                 print_error 'Option is unknown.'
             ;;
 
@@ -112,27 +111,25 @@ EOT
 
     shift $((OPTIND - 1)) || exit
 
-    print_verbose 'KEEP=%s' "${KEEP}"
+    print_verbose 'KEEP=%s' "$KEEP"
 
     print_verbose 'TAR_OPTIONS=(%s)' "${TAR_OPTIONS[*]}"
 
-
     for INFILE
     do
-        if [[ ! -e "${INFILE}" ]]
+        if [[ ! -e "$INFILE" ]]
         then
-            print_error '%q does not exist.' "${INFILE}"
+            print_error '%q does not exist.' "$INFILE"
         fi
 
-        print_verbose 'Untarring %q' "${INFILE}"
+        print_verbose 'Untarring %q' "$INFILE"
 
-        tar --extract --keep-old-files --one-top-level --file "${INFILE}" "${TAR_OPTIONS[@]}" || exit
+        tar --extract --keep-old-files --one-top-level --file "$INFILE" "${TAR_OPTIONS[@]}" || exit
 
-        if ! ${KEEP}
+        if ! $KEEP
         then
-            del -- "${INFILE}" || exit
+            del -- "$INFILE" || exit
         fi
     done
     )
 }
-
