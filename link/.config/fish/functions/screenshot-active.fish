a587a90721086b6f2455905ec45d115b0e3a4446
planet36 <planet36@users.noreply.github.com>
2021-06-18 07:57:06 -0400

    Add script to take screenshot of active window

---
+++ link/.config/fish/functions/screenshot-active.fish
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+function screenshot-active --description 'Take a screenshot of the active window'
+
+    set --local DATETIME (date -u +%Y%m%dT%H%M%SS%3N)
+
+    if command --quiet xdotool 
+
+        set --local WIN_ID (xdotool getactivewindow)
+
+    else if command --quiet xprop 
+
+        set --local WIN_ID (xprop -root _NET_ACTIVE_WINDOW | awk '{print $NF}')
+
+    else
+        echo "Error: No tool to get active window id" 1>&2
+        return 1
+    end
+
+    if command --quiet import
+
+        import -window $WIN_ID ~/screenshot-$DATETIME.png
+
+    if command --quiet maim
+
+        maim --window $WIN_ID ~/screenshot-$DATETIME.png
+
+    else if command --quiet xwd
+
+        xwd -id $WIN_ID -out ~/screenshot-$DATETIME.xwd || return
+
+        convert ~/screenshot-$DATETIME.xwd ~/screenshot-$DATETIME.png || return
+
+        rm --force -- ~/screenshot-$DATETIME.xwd
+
+    else
+        echo "Error: No tool to get screenshot" 1>&2
+        return 1
+    end
+end
