0b46e8403f52c4f3d3fdb7ec53e93e9e8471a66f
planet36 <planet36@users.noreply.github.com>
2021-06-25 14:31:54 -0400

    Also copy $GNUPGHOME and $PASSWORD_STORE_DIR

--- link/.config/fish/functions/t-private.fish
+++ link/.config/fish/functions/t-private.fish
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: Steven Ward
 # SPDX-License-Identifier: OSL-3.0
 
-function t-private --description 'tar private config files and .ssh folder'
+function t-private --description 'tar private config files and folders'
 
     pushd . || return
 
@@ -13,12 +13,25 @@ function t-private --description 'tar private config files and .ssh folder'
 
     #find .config -type f -not -empty -name 'private.*' | xargs cp --verbose --target-directory $TGT_DIR || return
     #for FILE in (find .config -type f -not -empty -name 'private.*')
-    for FILE in (find .config -type f -name 'private.*')
-        rsync -avR "$FILE" "$TGT_DIR" || return
+    #for FILE in (find .config -type f -name 'private.*')
+    #    rsync -avR "$FILE" "$TGT_DIR" || return
+    #end
+
+    find .config -type f -name 'private.*' | rsync -avR --files-from=- . "$TGT_DIR" || return
+
+    if test -d "$GNUPGHOME"
+        #cp --verbose --target-directory "$TGT_DIR" -- "$GNUPGHOME" || return
+        rsync -avR (realpath --relative-base="$HOME" "$GNUPGHOME") "$TGT_DIR" || return
+    end
+
+    if test -d "$PASSWORD_STORE_DIR"
+        #cp --verbose --target-directory "$TGT_DIR" -- "$PASSWORD_STORE_DIR" || return
+        rsync -avR (realpath --relative-base="$HOME" "$PASSWORD_STORE_DIR") "$TGT_DIR" || return
     end
 
     if test -d .ssh/
-        cp --verbose --target-directory "$TGT_DIR" -- .ssh || return
+        #cp --verbose --target-directory "$TGT_DIR" -- .ssh || return
+        rsync -avR .ssh "$TGT_DIR" || return
     end
 
     if dir_is_empty "$TGT_DIR"
