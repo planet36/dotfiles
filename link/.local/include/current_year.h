fea731f4529f5c4e99b58143f2a7e588b7975a44
planet36 <planet36@users.noreply.github.com>
2021-06-30 11:31:03 -0400

    Add current_year.h, current_year.hpp

---
+++ link/.local/include/current_year.h
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+#pragma once
+
+#include <time.h>
+
+static int current_year_local()
+{
+	const time_t t = time(NULL);
+	// https://en.cppreference.com/w/c/chrono/localtime
+	return localtime(&t)->tm_year + 1900;
+}
+
+static int current_year_utc()
+{
+	const time_t t = time(NULL);
+	// https://en.cppreference.com/w/c/chrono/gmtime
+	return gmtime(&t)->tm_year + 1900;
+}
