75a8d7635ac9cef26166350da1eca37f3845dbac
planet36 <planet36@users.noreply.github.com>
2021-07-21 13:53:26 -0400

    Add round_half_up.py

---
+++ link/.local/lib/python/round_half_up.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+
+import decimal
+
+DECIMAL_PLACES = [
+		decimal.Decimal(10)**0,
+		decimal.Decimal(10)**-1,
+		decimal.Decimal(10)**-2,
+		decimal.Decimal(10)**-3,
+		decimal.Decimal(10)**-4,
+		decimal.Decimal(10)**-5,
+		decimal.Decimal(10)**-6,
+		decimal.Decimal(10)**-7,
+		decimal.Decimal(10)**-8,
+		decimal.Decimal(10)**-9,
+		decimal.Decimal(10)**-10,
+		]
+
+def round_half_up(x) -> decimal.Decimal:
+	return decimal.Decimal(x).quantize(exp=DECIMAL_PLACES[0], rounding=decimal.ROUND_HALF_UP)
