bed98bb29af84e317ea853e8ed6596f73e20932d
planet36 <planet36@users.noreply.github.com>
2021-07-26 12:55:58 -0400

    Add wezterm.lua

---
+++ link/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm';
+
+return {
+  color_scheme = "Argonaut",
+  enable_scroll_bar = false,
+  font = wezterm.font("Hack"),
+  font_size = 13.0,
+  scrollback_lines = 10000,
+}
