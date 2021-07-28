f78a0ab449e53bab9f320188e67787da38a9d103
planet36 <planet36@users.noreply.github.com>
2021-07-28 13:44:33 -0400

    Add link to clang docs

--- link/.local/include/likely.h
+++ link/.local/include/likely.h
@@ -4,5 +4,6 @@
 # pragma once
 
 // https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html#index-_005f_005fbuiltin_005fexpect
+// https://llvm.org/docs/BranchWeightMetadata.html#built-in-expect-instructions
 #define likely(expr) __builtin_expect(!!(expr), 1)
 #define unlikely(expr) __builtin_expect(!!(expr), 0)
