fb0bfeff9949661e9bf6519c87b23cc6b8ea0583
planet36 <planet36@users.noreply.github.com>
2021-09-21 22:59:54 -0400

    Use std::is_same_v instead of std::same_as

--- link/.local/include/number.hpp
+++ link/.local/include/number.hpp
@@ -12,18 +12,18 @@
 
 #pragma once
 
-#include <concepts>
+#include <type_traits>
 
 // adapted from
 // https://www.sandordargo.com/blog/2021/03/24/concepts-in-real-life
 template <typename T>
 concept number = std::is_arithmetic_v<T>
-	&& !std::same_as<T, bool>
-	&& !std::same_as<T, char>
-	&& !std::same_as<T, signed char>
-	&& !std::same_as<T, unsigned char>
-	&& !std::same_as<T, char8_t>
-	&& !std::same_as<T, char16_t>
-	&& !std::same_as<T, char32_t>
-	&& !std::same_as<T, wchar_t>
+	&& !std::is_same_v<T, bool>
+	&& !std::is_same_v<T, char>
+	&& !std::is_same_v<T, signed char>
+	&& !std::is_same_v<T, unsigned char>
+	&& !std::is_same_v<T, char8_t>
+	&& !std::is_same_v<T, char16_t>
+	&& !std::is_same_v<T, char32_t>
+	&& !std::is_same_v<T, wchar_t>
 ;
