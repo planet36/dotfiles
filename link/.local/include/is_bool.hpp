af34694edc8e23ea20d65e7a9e8fa7d3630f233b
planet36 <planet36@users.noreply.github.com>
2021-09-22 21:47:14 -0400

    Add is_bool.hpp

---
+++ link/.local/include/is_bool.hpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Checks if a type is a bool type
+/**
+\file
+\author Steven Ward
+\sa https://en.cppreference.com/w/cpp/language/type
+*/
+
+#pragma once
+
+#include <type_traits>
+
+template <typename T>
+struct is_bool :
+std::bool_constant<std::is_same_v<bool, std::remove_cv_t<T>>>{};
+
+template <typename T>
+inline constexpr bool is_bool_v = is_bool<T>::value;
