f8bc600a0c3f0e4a92808d54dfc975f810ce1472
planet36 <planet36@users.noreply.github.com>
2021-09-29 17:16:14 -0400

    Add {float,int}_bytes.hpp

---
+++ link/.local/include/float_bytes.hpp
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// compile-time typing of floats based on a given byte size
+/**
+\file
+\author Steven Ward
+\sa https://peter.bloomfield.online/using-cpp-templates-for-size-based-type-selection/
+*/
+
+#pragma once
+
+#include <type_traits>
+
+template <unsigned int N>
+using float_bytes =
+std::conditional_t<N <= sizeof (float), float,
+	std::conditional_t<N <= sizeof (double), double,
+		std::conditional_t<N <= sizeof (long double), long double,
+			void
+		>
+	>
+>;
---
+++ link/.local/include/int_bytes.hpp
@@ -0,0 +1,46 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// compile-time typing of integers based on a given byte size
+/**
+\file
+\author Steven Ward
+\sa https://peter.bloomfield.online/using-cpp-templates-for-size-based-type-selection/
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+
+template <unsigned int N>
+using int_bytes =
+std::conditional_t<N <= sizeof (std::int8_t), std::int8_t,
+	std::conditional_t<N <= sizeof (std::int16_t), std::int16_t,
+		std::conditional_t<N <= sizeof (std::int32_t), std::int32_t,
+			std::conditional_t<N <= sizeof (std::int64_t), std::int64_t,
+#if defined(__int128)
+				std::conditional_t<N <= sizeof (__int128), __int128, void>
+#else
+				void
+#endif
+			>
+		>
+	>
+>;
+
+template <unsigned int N>
+using uint_bytes =
+std::conditional_t<N <= sizeof (std::uint8_t), std::uint8_t,
+	std::conditional_t<N <= sizeof (std::uint16_t), std::uint16_t,
+		std::conditional_t<N <= sizeof (std::uint32_t), std::uint32_t,
+			std::conditional_t<N <= sizeof (std::uint64_t), std::uint64_t,
+#if defined(__int128)
+				std::conditional_t<N <= sizeof (unsigned __int128), unsigned __int128, void>
+#else
+				void
+#endif
+			>
+		>
+	>
+>;
