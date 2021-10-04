56916860c19282e5442213316f0aff90b60f16b7
planet36 <planet36@users.noreply.github.com>
2021-10-04 11:36:53 -0400

    Add roundm.hpp

---
+++ link/.local/include/roundm.hpp
@@ -0,0 +1,93 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// round to a multiple of an integer
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <concepts>
+
+/**
+\pre \a m > 0
+*/
+template <std::integral T>
+T roundm_trunc(const T x, const T m)
+{
+	return x - x%m;
+	//return (x/m)*m;
+}
+
+/**
+\pre \a m > 0
+*/
+template <std::integral T>
+T roundm_floor(const T x, const T m)
+{
+	const auto t = roundm_trunc(x, m);
+
+	// necessary for x < 0
+	//if (x%m == 0)
+	if (x == t)
+		return x;
+
+	if (x < 0)
+		return t - m;
+	else
+		return t;
+}
+
+/**
+\pre \a m > 0
+*/
+template <std::integral T>
+T roundm_ceil(const T x, const T m)
+{
+	const auto t = roundm_trunc(x, m);
+
+	// necessary for x > 0
+	//if (x%m == 0)
+	if (x == t)
+		return x;
+
+	if (x < 0)
+		return t;
+	else
+		return t + m;
+}
+
+// away from zero
+/**
+\pre \a m > 0
+*/
+template <std::integral T>
+T roundm_up(const T x, const T m)
+{
+	const auto t = roundm_trunc(x, m);
+
+	//if (x%m == 0)
+	if (x == t)
+		return x;
+
+	if (x < 0)
+		return t - m;
+	else
+		return t + m;
+}
+
+/**
+\pre \a m > 0
+*/
+template <std::integral T>
+T roundm_nearest(const T x, const T m)
+{
+	const auto t = roundm_trunc(x, m);
+
+	if (x < 0)
+		return (x%m < -(m/2)) ? t - m : t;
+	else
+		return (x%m >  (m/2)) ? t + m : t;
+}
