be2a17c41203f4e6b7e710037153d0ccb8db7e87
planet36 <planet36@users.noreply.github.com>
2021-10-22 15:08:22 -0400

    Add overloads for quote function

--- link/.local/include/quote.hpp
+++ link/.local/include/quote.hpp
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 
 std::string quote(const std::string& s, const char delim = '"',
                   const char escape = '\\')
@@ -23,6 +24,38 @@ std::string quote(const std::string& s, const char delim = '"',
 		if (c == delim || c == escape)
 			result.push_back(escape);
 		result.push_back(c);
+	}
+	result.push_back(delim);
+	return result;
+}
+
+std::string quote(const std::string_view& s, const char delim = '"',
+                  const char escape = '\\')
+{
+	std::string result;
+	result.reserve(s.size() + 2);
+	result.push_back(delim);
+	for (const auto c : s)
+	{
+		if (c == delim || c == escape)
+			result.push_back(escape);
+		result.push_back(c);
+	}
+	result.push_back(delim);
+	return result;
+}
+
+std::string quote(const char* s, const char delim = '"',
+                  const char escape = '\\')
+{
+	std::string result;
+	result.push_back(delim);
+	while (*s)
+	{
+		if (*s == delim || *s == escape)
+			result.push_back(escape);
+		result.push_back(*s);
+		++s;
 	}
 	result.push_back(delim);
 	return result;
