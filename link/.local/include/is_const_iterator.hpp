c9b18484ddc2f490a2ecdadd52f69c25c111995d
planet36 <planet36@users.noreply.github.com>
2021-10-22 13:45:18 -0400

    Rename concept to const_iterator; create type trait named is_const_iterator

--- link/.local/include/is_const_iterator.hpp
+++ link/.local/include/is_const_iterator.hpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Steven Ward
 // SPDX-License-Identifier: OSL-3.0
 
-/// Concept for const iterator type
+/// Type trait and concept for const iterator type
 /**
 \file
 \author Steven Ward
@@ -12,5 +12,12 @@
 #include <iterator>
 #include <type_traits>
 
+template <typename Iter>
+struct is_const_iterator :
+std::bool_constant<!std::is_swappable_v<typename std::iterator_traits<Iter>::reference>>{};
+
+template <typename Iter>
+inline constexpr bool is_const_iterator_v = is_const_iterator<Iter>::value;
+
 template <class Iter>
-concept is_const_iterator = !std::is_swappable_v<typename std::iterator_traits<Iter>::reference>;
+concept const_iterator = is_const_iterator_v<Iter>;
