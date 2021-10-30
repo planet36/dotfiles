9ab9b8d507c31522252d01df1b20d42eda0980a7
planet36 <planet36@users.noreply.github.com>
2021-10-30 16:54:37 -0400

    Add concepts for Unordered Associative Container types

--- link/.local/include/associative_container.hpp
+++ link/.local/include/associative_container.hpp
@@ -1,12 +1,14 @@
 // SPDX-FileCopyrightText: Steven Ward
 // SPDX-License-Identifier: OSL-3.0
 
-/// Concept for Associative Container types
+/// Concepts for Associative Container types and Unordered Associative Container types
 /**
 \file
 \author Steven Ward
 \sa https://en.cppreference.com/w/cpp/named_req/AssociativeContainer
 \sa https://eel.is/c++draft/associative.reqmts
+\sa https://en.cppreference.com/w/cpp/named_req/UnorderedAssociativeContainer
+\sa https://eel.is/c++draft/unord.req
 */
 
 #pragma once
@@ -14,6 +16,8 @@
 #include <concepts>
 #include <map>
 #include <set>
+#include <unordered_map>
+#include <unordered_set>
 
 template <typename Container>
 concept AssociativeContainer =
@@ -21,4 +25,12 @@ concept AssociativeContainer =
 || std::derived_from<Container, std::multimap<typename Container::key_type, typename Container::mapped_type>>
 || std::derived_from<Container, std::set<typename Container::key_type>>
 || std::derived_from<Container, std::multiset<typename Container::key_type>>
+;
+
+template <typename Container>
+concept UnorderedAssociativeContainer =
+   std::derived_from<Container, std::unordered_map<typename Container::key_type, typename Container::mapped_type>>
+|| std::derived_from<Container, std::unordered_multimap<typename Container::key_type, typename Container::mapped_type>>
+|| std::derived_from<Container, std::unordered_set<typename Container::key_type>>
+|| std::derived_from<Container, std::unordered_multiset<typename Container::key_type>>
 ;
