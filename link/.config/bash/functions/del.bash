54d75c90cbc60a018ae1f565066684afac73a297
planet36 <planet36@users.noreply.github.com>
2021-11-03 10:53:56 -0400

    No need for options list delimiter

--- link/.config/bash/functions/del.bash
+++ link/.config/bash/functions/del.bash
@@ -6,5 +6,5 @@ function del
     #export SIMPLE_BACKUP_SUFFIX=$(date -u +.%Y%m%dT%H%M%SS%3N)
 
     #mv --verbose --backup=simple --suffix=.$(date -u +"%Y%m%dT%H%M%SS%3N") --target-directory "$TRASH_DIR"
-    command mv --verbose --backup=simple --suffix="$(date -u +.%Y%m%dT%H%M%SS%3N)" --target-directory "$TRASH_DIR" -- "${@}"
+    command mv --verbose --backup=simple --suffix="$(date -u +.%Y%m%dT%H%M%SS%3N)" --target-directory "$TRASH_DIR" "${@}"
 }
