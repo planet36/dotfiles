5ca79b81a33fe24ce53d1ac985da54f7ac5c1912
planet36 <planet36@users.noreply.github.com>
2021-11-07 21:33:42 -0500

    Add shell function git-remote-https-to-ssh

---
+++ link/.config/fish/functions/git-remote-https-to-ssh.fish
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+function git-remote-https-to-ssh --description 'change remote url from https to ssh'
+
+    echo "# Before"
+    git remote --verbose
+
+    git remote set-url origin (git remote get-url --all origin | sed -e 's|https://([^/]+)/|git@\1.com:|')
+    echo
+
+    echo "# After"
+    git remote --verbose
+end
