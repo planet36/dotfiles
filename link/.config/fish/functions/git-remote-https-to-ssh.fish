e0cf94e351c49f0603b2bc119b89ed3217604770
planet36 <planet36@users.noreply.github.com>
2021-11-15 20:35:55 -0500

    Fix typo in sed substitution

--- link/.config/fish/functions/git-remote-https-to-ssh.fish
+++ link/.config/fish/functions/git-remote-https-to-ssh.fish
@@ -6,7 +6,7 @@ function git-remote-https-to-ssh --description 'change remote url from https to 
     echo "# Before"
     git remote --verbose
 
-    git remote set-url origin (git remote get-url --all origin | sed -E -e 's|https://([^/]+)/|git@\1.com:|')
+    git remote set-url origin (git remote get-url --all origin | sed -E -e 's|https://([^/]+)/|git@\1:|')
     echo
 
     echo "# After"
