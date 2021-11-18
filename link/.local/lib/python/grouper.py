7e37e28191d71a53a63398ccac25d41742a65fde
planet36 <planet36@users.noreply.github.com>
2021-11-17 22:30:31 -0500

    Sync with source

--- link/.local/lib/python/grouper.py
+++ link/.local/lib/python/grouper.py
@@ -10,7 +10,7 @@ from itertools import zip_longest
 # copied from
 # https://docs.python.org/3/library/itertools.html#itertools-recipes
 def grouper(iterable, n, fillvalue=None):
-	"Collect data into fixed-length chunks or blocks"
-	# grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+	"Collect data into non-overlapping fixed-length chunks or blocks"
+	# grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
 	args = [iter(iterable)] * n
 	return zip_longest(*args, fillvalue=fillvalue)
