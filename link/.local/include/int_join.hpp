109142d76f857d6ea80d3ceae696ad6bd3ac4877
planet36 <planet36@users.noreply.github.com>
2021-11-24 15:19:46 -0500

    Add int_join.hpp

---
+++ link/.local/include/int_join.hpp
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Join smaller ints into larger ints
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <cstdint>
+
+uint16_t int_join(const uint8_t hi, const uint8_t lo)
+{
+	const union {
+		uint16_t whole;
+		uint8_t parts[2];
+	} u = {.parts = {lo, hi}};
+	return u.whole;
+}
+
+uint32_t int_join(const uint16_t hi, const uint16_t lo)
+{
+	const union {
+		uint32_t whole;
+		uint16_t parts[2];
+	} u = {.parts = {lo, hi}};
+	return u.whole;
+}
+
+uint64_t int_join(const uint32_t hi, const uint32_t lo)
+{
+	const union {
+		uint64_t whole;
+		uint32_t parts[2];
+	} u = {.parts = {lo, hi}};
+	return u.whole;
+}
