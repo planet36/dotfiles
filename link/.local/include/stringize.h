3bcf3a7547f4fa4c6c6f709bcdaef82fa2488dcc
planet36 <planet36@users.noreply.github.com>
2021-12-07 12:46:23 -0500

    Add stringize.h

---
+++ link/.local/include/stringize.h
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Stringize a macro argument
+/**
+\file
+\author Steven Ward
+\sa https://gcc.gnu.org/onlinedocs/cpp/Stringizing.html
+\sa https://en.wikipedia.org/wiki/C_preprocessor#Token_stringification
+*/
+
+# pragma once
+
+#define XSTR(s) STR(s)
+#define STR(s) #s
