b9a57458914a7ef3f8cd7a7ec5824f042690999a
planet36 <planet36@users.noreply.github.com>
2022-01-06 13:31:13 -0500

    Add quote functions
    These quote functions correspond to the behaviors of the ls option '--quoting-style'
    https://www.man7.org/linux/man-pages/man1/ls.1.html

--- link/.local/include/quote.hpp
+++ link/.local/include/quote.hpp
@@ -10,6 +10,9 @@
 
 #pragma once
 
+#include "ascii.hpp"
+
+#include <cctype>
 #include <string>
 #include <string_view>
 
@@ -115,5 +118,79 @@ std::string quote(const char* s, const char delim = double_quote,
 		++s;
 	}
 	result.push_back(delim);
+	return result;
+}
+
+std::string quote_shell_always(const std::string& s,
+                               const char delim = single_quote)
+{
+	std::string result;
+	result.reserve(s.size() + 2);
+	result.push_back(delim);
+	for (const char c : s)
+	{
+		if (c == delim)
+			result += single_quote_escaped;
+		else
+			result.push_back(c);
+	}
+	result.push_back(delim);
+	return result;
+}
+
+std::string quote_shell(const std::string& s)
+{
+	return contains_shell_special_chars(s) ? quote_shell_always(s) : s;
+}
+
+std::string quote_escape(const std::string& s, const char escape = backslash)
+{
+	std::string result;
+	result.reserve(s.size());
+	for (const char c : s)
+	{
+		if (std::iscntrl(c))
+			result += ascii_cntrl_simple_escape_seq_oct[c];
+		else
+		{
+			if ((c == ' ') || (c == escape))
+				result.push_back(escape);
+			result.push_back(c);
+		}
+	}
+	return result;
+}
+
+std::string quote_c(const std::string& s, const char delim = double_quote,
+                    const char escape = backslash)
+{
+	std::string result;
+	result.reserve(s.size() + 2);
+	result.push_back(delim);
+	for (const char c : s)
+	{
+		if (std::iscntrl(c))
+			result += ascii_cntrl_simple_escape_seq_oct[c];
+		else
+		{
+			if ((c == delim) || (c == escape))
+				result.push_back(escape);
+			result.push_back(c);
+		}
+	}
+	result.push_back(delim);
+	return result;
+}
+
+std::string quote_pcre(const std::string& s)
+{
+	std::string result;
+	result.reserve(s.size());
+	for (const char c : s)
+	{
+		if (!isword(c))
+			result.push_back(backslash);
+		result.push_back(c);
+	}
 	return result;
 }
