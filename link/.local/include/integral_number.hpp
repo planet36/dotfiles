36b94c116701b584c1ae39fdb6495c6350349404
planet36 <planet36@users.noreply.github.com>
2022-01-11 00:10:28 -0500

    Add type_any_of.hpp, integral_number.hpp

---
+++ link/.local/include/integral_number.hpp
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Concept for an integral number type
+/**
+\file
+\author Steven Ward
+\sa https://en.cppreference.com/w/cpp/language/type
+*/
+
+#pragma once
+
+#include "type_any_of.hpp"
+
+#include <type_traits>
+
+template <typename T>
+struct is_integral_number : is_type_any_of<std::remove_cv_t<T>,
+	short,
+	int,
+	long,
+	long long,
+	unsigned short,
+	unsigned int,
+	unsigned long,
+	unsigned long long>{};
+
+template <typename T>
+inline constexpr bool is_integral_number_v = is_integral_number<T>::value;
+
+template<typename T>
+concept integral_number = is_integral_number_v<T>;
---
+++ link/.local/include/type_any_of.hpp
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Concept to check if a type is the same as any other given types
+/**
+\file
+\author Steven Ward
+\sa https://en.cppreference.com/w/cpp/concepts/same_as
+*/
+
+#pragma once
+
+#include <type_traits>
+
+template <typename T, typename ... U>
+struct is_type_any_of : std::bool_constant<(std::is_same_v<T, U> || ...)>{};
+
+template <typename T, typename ... U>
+inline constexpr bool is_type_any_of_v = is_type_any_of<T>::value;
+
+template <typename T, typename ... U>
+concept type_any_of = is_type_any_of_v<T, U...>;
