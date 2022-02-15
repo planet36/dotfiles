d2ba0ff6fa5cfee5cbd08ed8e340b122f2aec257
planet36 <planet36@users.noreply.github.com>
2022-02-15 10:23:54 -0500

    Add example commands to diff and commit generated src files

--- other/generate-src/generate-files.bash
+++ other/generate-src/generate-files.bash
@@ -9,9 +9,19 @@ set -e
 
 cd "$SCRIPT_DIR"
 
-OUT_DIR="$HOME"/.local/include
+OUT_DIR="$(git rev-parse --show-toplevel)"/link/.local/include
 
 calc -d -m 4 -f generate-series-approx-coeff-aux-lat.cal > "$OUT_DIR"/aux-lat-conv.hpp
 calc -d -m 4 -f generate-series-approx-coeff-tm.cal      > "$OUT_DIR"/utm-ups-const.hpp
 python3 generate-gcc-machine-modes-typedefs.py           > "$OUT_DIR"/gcc-machine-modes-typedefs.hpp
 python3 generate-math-const.py                           > "$OUT_DIR"/math-const.hpp
+
+cat <<EOT
+# Run these commands:
+
+git diff \
+$OUT_DIR/{aux-lat-conv.hpp,gcc-machine-modes-typedefs.hpp,math-const.hpp,utm-ups-const.hpp}
+
+git commit -m 'Update generated source code files' \
+$OUT_DIR/{aux-lat-conv.hpp,gcc-machine-modes-typedefs.hpp,math-const.hpp,utm-ups-const.hpp}
+EOT
