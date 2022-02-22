1d2bdb32792d93eead43fa1fd7bbef9f73c3869d
planet36 <planet36@users.noreply.github.com>
2022-02-22 15:54:21 -0500

    Move up int_div_floor, int_div_ceil, int_div_round

--- link/.local/include/int_div.hpp
+++ link/.local/include/int_div.hpp
@@ -24,42 +24,6 @@ constexpr auto iabs(const std::unsigned_integral auto x)
 	return x;
 }
 
-/// get the quotient of the _floored_ integer division
-/**
-\note Division by zero is not checked.
-\note Overflow is not checked.
-*/
-constexpr auto int_div_floor(const std::signed_integral auto x,
-                             const std::signed_integral auto y)
-{
-	return (x / y) - (!same_sign(x, y) && ((x % y) != 0));
-}
-
-/// get the quotient of the _ceiling_ integer division
-/**
-\note Division by zero is not checked.
-\note Overflow is not checked.
-*/
-constexpr auto int_div_ceil(const std::signed_integral auto x,
-                            const std::signed_integral auto y)
-{
-	return (x / y) + (same_sign(x, y) && ((x % y) != 0));
-}
-
-/// get the quotient of the _rounded_ integer division
-/**
-\note Division by zero is not checked.
-\note Overflow is not checked.
-*/
-constexpr auto int_div_round(const std::signed_integral auto x,
-                             const std::signed_integral auto y)
-{
-	return (x / y) + sign(x % y) * sign(y) * (
-	           (iabs(x % y) > iabs(y / 2)) ||
-	           ((iabs(x % y) == iabs(y / 2)) && ((y % 2) == 0))
-	       );
-}
-
 /// get the quotient and remainder of the _floored_ integer division
 /**
 \note Division by zero is not checked.
@@ -186,4 +150,40 @@ constexpr void int_divmod_round(const std::signed_integral auto x,
 		}
 	}
 #endif
+}
+
+/// get the quotient of the _floored_ integer division
+/**
+\note Division by zero is not checked.
+\note Overflow is not checked.
+*/
+constexpr auto int_div_floor(const std::signed_integral auto x,
+                             const std::signed_integral auto y)
+{
+	return (x / y) - (!same_sign(x, y) && ((x % y) != 0));
+}
+
+/// get the quotient of the _ceiling_ integer division
+/**
+\note Division by zero is not checked.
+\note Overflow is not checked.
+*/
+constexpr auto int_div_ceil(const std::signed_integral auto x,
+                            const std::signed_integral auto y)
+{
+	return (x / y) + (same_sign(x, y) && ((x % y) != 0));
+}
+
+/// get the quotient of the _rounded_ integer division
+/**
+\note Division by zero is not checked.
+\note Overflow is not checked.
+*/
+constexpr auto int_div_round(const std::signed_integral auto x,
+                             const std::signed_integral auto y)
+{
+	return (x / y) + sign(x % y) * sign(y) * (
+	           (iabs(x % y) > iabs(y / 2)) ||
+	           ((iabs(x % y) == iabs(y / 2)) && ((y % 2) == 0))
+	       );
 }
