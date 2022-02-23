66242e0f9a1420351d9f4bd76cccdea7e1d89ae7
planet36 <planet36@users.noreply.github.com>
2022-02-23 12:18:31 -0500

    Move functions from div.hpp, mod.hpp to int_div.hpp

--- link/.local/include/div.hpp
+++ link/.local/include/div.hpp
@@ -11,33 +11,3 @@
 
 #include <cmath>
 #include <concepts>
-
-/// get the quotient and remainder of the _truncated_ integer division
-/**
-\note Division by zero is not checked.
-\note Overflow is not checked.
-*/
-constexpr void trunc_div_mod(const std::integral auto x,
-                   const std::integral auto y,
-                   std::integral auto& quo,
-                   std::integral auto& rem)
-{
-	quo = x / y;
-	rem = x % y;
-}
-
-/// get the quotient and remainder of the _truncated_ division
-/**
-\note Division by zero is not checked.
-\note Overflow is not checked.
-*/
-constexpr void trunc_div_mod(const std::floating_point auto x,
-                   const std::floating_point auto y,
-                   std::floating_point auto& quo,
-                   std::floating_point auto& rem)
-{
-	quo = x / y;
-	rem = x - std::trunc(x / y) * y;
-	// fmod is less accurate than division and trunc
-	//rem = std::fmod(x, y);
-}
--- link/.local/include/int_div.hpp
+++ link/.local/include/int_div.hpp
@@ -14,6 +14,36 @@
 #include <cmath>
 #include <concepts>
 
+/// get the quotient and remainder of the _truncated_ integer division
+/**
+\note Division by zero is not checked.
+\note Overflow is not checked.
+*/
+constexpr void trunc_div_mod(const std::integral auto x,
+                   const std::integral auto y,
+                   std::integral auto& quo,
+                   std::integral auto& rem)
+{
+	quo = x / y;
+	rem = x % y;
+}
+
+/// get the quotient and remainder of the _truncated_ division
+/**
+\note Division by zero is not checked.
+\note Overflow is not checked.
+*/
+constexpr void trunc_div_mod(const std::floating_point auto x,
+                   const std::floating_point auto y,
+                   std::floating_point auto& quo,
+                   std::floating_point auto& rem)
+{
+	quo = x / y;
+	rem = x - std::trunc(x / y) * y;
+	// fmod is less accurate than division and trunc
+	//rem = std::fmod(x, y);
+}
+
 constexpr auto iabs(const std::signed_integral auto x)
 {
 	return std::abs(x);
@@ -186,4 +216,82 @@ constexpr auto round_div(const std::signed_integral auto x,
 	           (iabs(x % y) > iabs(y / 2)) ||
 	           ((iabs(x % y) == iabs(y / 2)) && ((y % 2) == 0))
 	       );
+}
+
+/// get the remainder of the _floored_ integer division
+/**
+\note Division by zero is not checked.
+\note Overflow is not checked.
+\sa http://python-history.blogspot.com/2010/08/why-pythons-integer-division-floors.html
+\sa https://eel.is/c++draft/expr.mul#4
+\sa https://stackoverflow.com/a/3602857
+\sa https://reference.wolfram.com/language/ref/Quotient.html
+\sa https://www.wolframalpha.com/input?i=Quotient%5B9%2C+5%5D%2C+Quotient%5B-9%2C+5%5D%2C+Quotient%5B9%2C+-5%5D%2C+Quotient%5B-9%2C+-5%5D
+\sa https://reference.wolfram.com/language/ref/Mod.html
+\sa https://www.wolframalpha.com/input?i=Mod%5B9%2C+5%5D%2C+Mod%5B-9%2C+5%5D%2C+Mod%5B9%2C+-5%5D%2C+Mod%5B-9%2C+-5%5D
+
+In C, integer division is truncated, not floored.  So the "%" operator can't be
+used without adjustment.
+
+Example of truncated division:
+\verbatim
+ 9 /  5 =  1
+-9 /  5 = -1
+ 9 / -5 = -1
+-9 / -5 =  1
+
+ 9 %  5 =  4
+-9 %  5 = -4
+ 9 % -5 =  4
+-9 % -5 = -4
+
+x % y == x - y * (x / y)
+\endverbatim
+
+Example of floored division:
+\verbatim
+ 9 /  5 ==  1
+-9 /  5 == -2
+ 9 / -5 == -2
+-9 / -5 ==  1
+
+ 9 mod  5 ==  4
+-9 mod  5 ==  1
+ 9 mod -5 == -1
+-9 mod -5 == -4
+
+Mod[x, y] == x - y * Quotient[x, y]
+\endverbatim
+*/
+constexpr auto floor_mod(const std::signed_integral auto x,
+                   const std::signed_integral auto y)
+{
+#if 0
+	return x - y * floor_div(x, y);
+#else
+	auto rem = x % y;
+	if (y < 0)
+	{
+		if (rem > 0)
+		{
+			rem += y;
+		}
+	}
+	else
+	{
+		if (rem < 0)
+		{
+			rem += y;
+		}
+	}
+
+	return rem;
+#endif
+}
+
+/// get the adjusted remainder of the _floored_ integer division
+constexpr auto floor_amod(const std::signed_integral auto x,
+                    const std::signed_integral auto y)
+{
+	return floor_mod(x, y) != 0 ? floor_mod(x, y) : y;
 }
