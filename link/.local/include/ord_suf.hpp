f487b208ddba122b9963624da8971ab38bc2fad5
planet36 <planet36@users.noreply.github.com>
2022-02-24 12:29:26 -0500

    Add ord_suf.hpp

---
+++ link/.local/include/ord_suf.hpp
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Get the English ordinal suffix for an integer
+/**
+\file
+\author Steven Ward
+\sa https://en.wikipedia.org/wiki/English_numerals#Ordinal_numbers
+\sa https://en.wikipedia.org/wiki/Ordinal_indicator#English
+\sa https://en.wikipedia.org/wiki/Ordinal_numeral#English
+*/
+
+#pragma once
+
+#include "abs.hpp"
+
+#include <concepts>
+
+const char* ordinal_suffix(std::unsigned_integral auto n)
+{
+	switch (n %= 100)
+	{
+	case 11:
+	case 12:
+	case 13:
+		return "th";
+	default: break;
+	}
+
+	switch (n %= 10)
+	{
+	case 1: return "st";
+	case 2: return "nd";
+	case 3: return "rd";
+	default: break;
+	}
+
+	return "th";
+}
+
+const char* ordinal_suffix(const std::signed_integral auto n)
+{
+	return ordinal_suffix(uabs(n));
+}
