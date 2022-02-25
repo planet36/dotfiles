4d3db26b3c3db93217b8af68e834c2e64b48372b
planet36 <planet36@users.noreply.github.com>
2022-02-25 13:51:48 -0500

    Include cstdlib instead of cmath

--- link/.local/include/abs.hpp
+++ link/.local/include/abs.hpp
@@ -9,16 +9,18 @@
 
 #pragma once
 
-#include <cmath>
 #include <concepts>
+#include <cstdlib>
 #include <type_traits>
 
-constexpr auto iabs(const std::signed_integral auto x)
+constexpr auto
+iabs(const std::signed_integral auto x)
 {
 	return std::abs(x);
 }
 
-constexpr auto iabs(const std::unsigned_integral auto x)
+constexpr auto
+iabs(const std::unsigned_integral auto x)
 {
 	return x;
 }
@@ -26,7 +28,8 @@ constexpr auto iabs(const std::unsigned_integral auto x)
 /**
 \return the absolute value of \a x as an unsigned type
 */
-constexpr auto uabs(const std::signed_integral auto x) -> std::make_unsigned_t<decltype(x)>
+constexpr auto
+uabs(const std::signed_integral auto x) -> std::make_unsigned_t<decltype(x)>
 {
 	return std::abs(x);
 }
@@ -34,7 +37,8 @@ constexpr auto uabs(const std::signed_integral auto x) -> std::make_unsigned_t<d
 /**
 \return the absolute value of \a x as an unsigned type
 */
-constexpr auto uabs(const std::unsigned_integral auto x)
+constexpr auto
+uabs(const std::unsigned_integral auto x)
 {
 	return x;
 }
