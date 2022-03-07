be4f47955d8573838d703ef0892918ad4927e49f
planet36 <planet36@users.noreply.github.com>
2022-03-07 12:54:10 -0500

    Do not give -i option to bash

--- link/.config/fish/functions/bashi.fish
+++ link/.config/fish/functions/bashi.fish
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: OSL-3.0
 
 function bashi --wraps bash --description 'bash that uses config.bash'
-    command bash -i --rcfile "$XDG_CONFIG_HOME"/bash/config.bash $argv
+    command bash --rcfile "$XDG_CONFIG_HOME"/bash/config.bash $argv
 end
