f419c7ea85dcac0c3800f0bb11d79f9277fb193d
planet36 <planet36@users.noreply.github.com>
2022-03-09 12:55:39 -0500

    Change gpp, gccc functions into scripts

--- link/.config/bash/functions/gccc.bash
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Steven Ward
-# SPDX-License-Identifier: OSL-3.0
-
-function gccc
-{
-    # shellcheck disable=SC2086
-    command gcc $CPPFLAGS $CFLAGS "${@}"
-}
--- link/.config/bash/functions/gpp.bash
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Steven Ward
-# SPDX-License-Identifier: OSL-3.0
-
-function gpp
-{
-    # shellcheck disable=SC2086
-    command g++ $CPPFLAGS $CXXFLAGS "${@}" -lfmt
-}
