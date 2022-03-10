706cc2b27129c328dad2f915e30bc10fe16df050
planet36 <planet36@users.noreply.github.com>
2022-03-10 11:23:53 -0500

    Consolidate sign functions

--- link/.local/include/sign.hpp
+++ link/.local/include/sign.hpp
@@ -11,12 +11,7 @@ This is also called "signum" or "sgn".
 \sa https://mathworld.wolfram.com/Sign.html
 \sa https://reference.wolfram.com/language/ref/Sign.html
 
-For an integer \c x, <code>sign(x)</code> returns
-\li \c 1, if <code>x > 0</code>
-\li \c -1, if <code>x < 0</code>
-\li \c 0, otherwise
-
-For a floating point \c x, <code>sign(x)</code> returns
+For a number \c x, <code>sign(x)</code> returns
 \li \c 1, if <code>x > 0</code>
 \li \c -1, if <code>x < 0</code>
 \li \c 0, otherwise (including \c NaN)
@@ -29,22 +24,7 @@ Two numbers have the same sign if both are non-positive or non-negative.
 #include <concepts>
 #include <type_traits>
 
-constexpr int sign(const std::unsigned_integral auto x)
-{
-	return (x > 0) ? 1 : ((x < 0) ? -1 : 0);
-}
-
-constexpr int sign(const std::signed_integral auto x)
-{
-	return (x > 0) ? 1 : ((x < 0) ? -1 : 0);
-}
-
-#if 0
-template <std::floating_point T>
-constexpr T sign(const T x)
-#else
-constexpr int sign(const std::floating_point auto x)
-#endif
+constexpr int sign(const auto x)
 {
 	return (x > 0) ? 1 : ((x < 0) ? -1 : 0);
 }
