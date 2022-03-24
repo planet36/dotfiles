bb5333715cc67ee50af144578c7fca5286814882
planet36 <planet36@users.noreply.github.com>
2022-03-24 12:31:38 -0500

    Move detailed comment to the top

--- link/.local/include/div_mod_i.hpp
+++ link/.local/include/div_mod_i.hpp
@@ -7,6 +7,47 @@
 \author Steven Ward
 \note Division by zero is not checked.
 \note Overflow is not checked.
+
+\sa http://python-history.blogspot.com/2010/08/why-pythons-integer-division-floors.html
+\sa https://eel.is/c++draft/expr.mul#4
+\sa https://stackoverflow.com/a/3602857
+\sa https://reference.wolfram.com/language/ref/Quotient.html
+\sa https://www.wolframalpha.com/input?i=Quotient%5B9%2C+5%5D%2C+Quotient%5B-9%2C+5%5D%2C+Quotient%5B9%2C+-5%5D%2C+Quotient%5B-9%2C+-5%5D
+\sa https://reference.wolfram.com/language/ref/Mod.html
+\sa https://www.wolframalpha.com/input?i=Mod%5B9%2C+5%5D%2C+Mod%5B-9%2C+5%5D%2C+Mod%5B9%2C+-5%5D%2C+Mod%5B-9%2C+-5%5D
+
+In C, integer division is truncated, not floored.  So the "%" operator can't be
+used without adjustment.
+
+Example of truncated division:
+\verbatim
+ 9 /  5 =  1
+-9 /  5 = -1
+ 9 / -5 = -1
+-9 / -5 =  1
+
+ 9 %  5 =  4
+-9 %  5 = -4
+ 9 % -5 =  4
+-9 % -5 = -4
+
+x % y == x - y * (x / y)
+\endverbatim
+
+Example of floored division:
+\verbatim
+ 9 /  5 ==  1
+-9 /  5 == -2
+ 9 / -5 == -2
+-9 / -5 ==  1
+
+ 9 mod  5 ==  4
+-9 mod  5 ==  1
+ 9 mod -5 == -1
+-9 mod -5 == -4
+
+Mod[x, y] == x - y * Quotient[x, y]
+\endverbatim
 */
 
 #pragma once
@@ -232,48 +273,6 @@ trunc_mod(const std::signed_integral auto x,
 }
 
 /// get the remainder of the _floored_ integer division
-/**
-\sa http://python-history.blogspot.com/2010/08/why-pythons-integer-division-floors.html
-\sa https://eel.is/c++draft/expr.mul#4
-\sa https://stackoverflow.com/a/3602857
-\sa https://reference.wolfram.com/language/ref/Quotient.html
-\sa https://www.wolframalpha.com/input?i=Quotient%5B9%2C+5%5D%2C+Quotient%5B-9%2C+5%5D%2C+Quotient%5B9%2C+-5%5D%2C+Quotient%5B-9%2C+-5%5D
-\sa https://reference.wolfram.com/language/ref/Mod.html
-\sa https://www.wolframalpha.com/input?i=Mod%5B9%2C+5%5D%2C+Mod%5B-9%2C+5%5D%2C+Mod%5B9%2C+-5%5D%2C+Mod%5B-9%2C+-5%5D
-
-In C, integer division is truncated, not floored.  So the "%" operator can't be
-used without adjustment.
-
-Example of truncated division:
-\verbatim
- 9 /  5 =  1
--9 /  5 = -1
- 9 / -5 = -1
--9 / -5 =  1
-
- 9 %  5 =  4
--9 %  5 = -4
- 9 % -5 =  4
--9 % -5 = -4
-
-x % y == x - y * (x / y)
-\endverbatim
-
-Example of floored division:
-\verbatim
- 9 /  5 ==  1
--9 /  5 == -2
- 9 / -5 == -2
--9 / -5 ==  1
-
- 9 mod  5 ==  4
--9 mod  5 ==  1
- 9 mod -5 == -1
--9 mod -5 == -4
-
-Mod[x, y] == x - y * Quotient[x, y]
-\endverbatim
-*/
 constexpr auto
 floor_mod(const std::signed_integral auto x,
           const std::signed_integral auto y)
