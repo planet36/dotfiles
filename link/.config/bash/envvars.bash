9d02da3eec137ba1c30ff7716e4a389d6fabf796
planet36 <planet36@users.noreply.github.com>
2022-04-12 13:36:09 -0500

    Compile in gnu++XX mode to get correct 128-bit integer limits/traits
    https://quuxplusone.github.io/blog/2019/02/28/is-int128-integral/
    """
    If you compile in -std=gnu++XX mode, libstdc++ will treat __int128 and
    unsigned __int128 as integral types with the proper numeric limits; but
    if you compile in the usual -std=c++XX mode, libstdc++ will not do any
    of that.
    
    On the other hand, libc++ treats __int128 as an integer type, full stop.
    """

--- link/.config/bash/envvars.bash
+++ link/.config/bash/envvars.bash
@@ -189,8 +189,8 @@ export PROFILE_OPTIONS="$PERF_TEST_OPTIONS -pg"
 # https://gcc.gnu.org/onlinedocs/cpp/Invocation.html
 export CPPFLAGS="-iquote $HOME/.local/include"
 
-export CFLAGS="$GCC_COMMON_OPTIONS -std=c2x"
-export CXXFLAGS="$GCC_COMMON_OPTIONS -std=c++23 -fchar8_t -fdiagnostics-show-template-tree -Wctor-dtor-privacy -Wextra-semi -Wmismatched-tags -Wmultiple-inheritance -Wnon-virtual-dtor -Wold-style-cast -Woverloaded-virtual -Wredundant-tags -Wsign-promo -Wstrict-null-sentinel -Wsuggest-final-methods -Wsuggest-final-types -Wsuggest-override -Wuseless-cast -Wzero-as-null-pointer-constant"
+export CFLAGS="$GCC_COMMON_OPTIONS -std=gnu2x"
+export CXXFLAGS="$GCC_COMMON_OPTIONS -std=gnu++23 -fchar8_t -fdiagnostics-show-template-tree -Wctor-dtor-privacy -Wextra-semi -Wmismatched-tags -Wmultiple-inheritance -Wnon-virtual-dtor -Wold-style-cast -Woverloaded-virtual -Wredundant-tags -Wsign-promo -Wstrict-null-sentinel -Wsuggest-final-methods -Wsuggest-final-types -Wsuggest-override -Wuseless-cast -Wzero-as-null-pointer-constant"
 
 # }}}
 
