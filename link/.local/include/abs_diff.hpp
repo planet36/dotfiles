c2b29be537f51d3109cc1d57b8751bc98ecac915
planet36 <planet36@users.noreply.github.com>
2022-04-15 09:26:13 -0500

    Add abs_diff.hpp

---
+++ link/.local/include/abs_diff.hpp
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Absolute difference between 2 integers as an unsigned integer
+/**
+\file
+\author Steven Ward
+\sa https://doc.rust-lang.org/stable/std/index.html?search=abs_diff&filter-crate=std
+*/
+
+#pragma once
+
+#include <concepts>
+#include <type_traits>
+
+template <std::unsigned_integral T, std::unsigned_integral U>
+constexpr
+std::common_type_t<T, U>
+abs_diff(const T a, const U b)
+{
+	return (a < b) ? b - a : a - b;
+}
+
+template <std::signed_integral T, std::signed_integral U>
+constexpr
+std::make_unsigned_t<std::common_type_t<T, U>>
+abs_diff(const T a, const U b)
+{
+	return (a < b) ? b - a : a - b;
+}
