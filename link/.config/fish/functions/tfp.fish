8a5258f724d3625ba089f4d70566e7216cb3ee1a
planet36 <planet36@users.noreply.github.com>
2022-05-12 10:33:11 -0500

    If git format-patch fails, exit
    Accept a destination directory to which the tar file will be moved

--- link/.config/fish/functions/tfp.fish
+++ link/.config/fish/functions/tfp.fish
@@ -3,6 +3,8 @@
 
 function tfp --description 'tar format patch'
 
+    set DST $argv[1]
+
     set PATCHES_DIR $(basename $(git rev-parse --show-toplevel)) || return
 
     # XXX: string replace returns 1 (failure) if no replacement was performed
@@ -13,11 +15,18 @@ function tfp --description 'tar format patch'
     # NOTE: If git format-patch gives this error
     # "fatal: base commit should be the ancestor of revision list"
     # Then remove "--base=auto" from the command.
-    git format-patch -o "$PATCHES_DIR" --keep-subject --base=auto --progress $(git rev-parse --abbrev-ref HEAD@{upstream})..HEAD || return
+    if not git format-patch -o "$PATCHES_DIR" --keep-subject --base=auto --progress $(git rev-parse --abbrev-ref HEAD@{upstream})..HEAD
+        rmdir --verbose -- "$PATCHES_DIR" || return
+        return 1
+    end
 
     if dir_is_empty "$PATCHES_DIR"
         rmdir --verbose -- "$PATCHES_DIR" || return
     else
         t "$PATCHES_DIR" && del "$PATCHES_DIR" || return
+
+        if test -d "$DST"
+            mv --verbose -- "$PATCHES_DIR".tar "$DST"
+        end
     end
 end
