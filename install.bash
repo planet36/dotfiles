6ea8c957e86412aaf0bb62df541936cf573b5467
planet36 <planet36@users.noreply.github.com>
2022-05-13 18:53:04 -0500

    Add XDG_STATE_HOME environment variable

--- install.bash
+++ install.bash
@@ -348,16 +348,19 @@ function setup_xdg_vars
     : "${XDG_CONFIG_HOME:=$HOME/.config}"
     : "${XDG_DATA_DIRS:=/usr/local/share/:/usr/share/}"
     : "${XDG_DATA_HOME:=$HOME/.local/share}"
+    : "${XDG_STATE_HOME:=$HOME/.local/state}"
 
     export XDG_CACHE_HOME
     export XDG_CONFIG_DIRS
     export XDG_CONFIG_HOME
     export XDG_DATA_DIRS
     export XDG_DATA_HOME
+    export XDG_STATE_HOME
 
     mkdir --verbose --parents -- "$XDG_CACHE_HOME"
     mkdir --verbose --parents -- "$XDG_CONFIG_HOME"
     mkdir --verbose --parents -- "$XDG_DATA_HOME"
+    mkdir --verbose --parents -- "$XDG_STATE_HOME"
 
     # XDG environment variables have been set
     # XDG base directories have been created
