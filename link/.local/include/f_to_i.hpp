75cd9134bc13228de843b91ee311b746bb851691
planet36 <planet36@users.noreply.github.com>
2022-05-23 15:08:42 -0500

    Add f_to_i.hpp

---
+++ link/.local/include/f_to_i.hpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Cast floating point to integer of sufficient size
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <limits>
+
+constexpr auto
+f_to_i(const float x)
+{
+	using result_type = int32_t;
+	static_assert(std::numeric_limits<decltype(x)>::digits <
+	              std::numeric_limits<result_type>::digits);
+	return static_cast<result_type>(x);
+}
+
+constexpr auto
+f_to_i(const double x)
+{
+	using result_type = int64_t;
+	static_assert(std::numeric_limits<decltype(x)>::digits <
+	              std::numeric_limits<result_type>::digits);
+	return static_cast<result_type>(x);
+}
