303bda4afba68a368361e02a07648ebac3dd35f1
planet36 <planet36@users.noreply.github.com>
2022-05-26 11:27:19 -0500

    Add i_to_f.hpp

---
+++ link/.local/include/i_to_f.hpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Cast integer to floating point of sufficient precision
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <concepts>
+#include <limits>
+
+template <std::integral T>
+requires (std::numeric_limits<T>::digits <= std::numeric_limits<float>::digits)
+constexpr auto
+i_to_f(const T x) {return static_cast<float>(x);}
+
+template <std::integral T>
+requires (std::numeric_limits<T>::digits >  std::numeric_limits<float>::digits) &&
+         (std::numeric_limits<T>::digits <= std::numeric_limits<double>::digits)
+constexpr auto
+i_to_f(const T x) {return static_cast<double>(x);}
+
+// XXX: Every uint64_t (digits=64) cannot be cast exactly to long double (digits=64)
+template <std::integral T>
+requires (std::numeric_limits<T>::digits >  std::numeric_limits<double>::digits) &&
+         (std::numeric_limits<T>::digits <= std::numeric_limits<long double>::digits)
+constexpr auto
+i_to_f(const T x) {return static_cast<long double>(x);}
