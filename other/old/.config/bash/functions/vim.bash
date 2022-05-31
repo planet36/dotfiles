027dd3715f9673be63cdab58dc298dc8a54c5e58
planet36 <planet36@users.noreply.github.com>
2022-05-31 08:31:42 -0500

    Remove vim files

--- other/old/.config/bash/functions/vim.bash
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Steven Ward
-# SPDX-License-Identifier: OSL-3.0
-
-function vim
-{
-    # shellcheck disable=SC2016
-    MYVIMRC='$XDG_CONFIG_HOME/vim/init.vim' VIMINIT='source $MYVIMRC' command vim "${@}"
-}
