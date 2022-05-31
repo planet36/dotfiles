8642a5b1126a7179bc859a95d61d9808b0f1f854
planet36 <planet36@users.noreply.github.com>
2022-05-31 16:48:19 -0500

    Create $TRASH_DIR if it doesn't exist

--- link/.config/bash/functions/del.bash
+++ link/.config/bash/functions/del.bash
@@ -3,6 +3,11 @@
 
 function del
 {
+    if [[ ! -d "$TRASH_DIR" ]]
+    then
+        mkdir --verbose --mode=0700 -- "$TRASH_DIR" || return
+    fi
+
     #export SIMPLE_BACKUP_SUFFIX=$(date -u +.%Y%m%dT%H%M%SS%3N)
 
     #mv --verbose --backup=simple --suffix=.$(date -u +"%Y%m%dT%H%M%SS%3N") --target-directory "$TRASH_DIR"
--- link/.config/fish/functions/del.fish
+++ link/.config/fish/functions/del.fish
@@ -3,6 +3,10 @@
 
 function del --wraps mv --description 'delete (move to trash folder)'
 
+    if not test -d "$TRASH_DIR"
+        mkdir --verbose --mode=0700 -- "$TRASH_DIR" || return
+    end
+
     #set -x SIMPLE_BACKUP_SUFFIX $(date -u +.%Y%m%dT%H%M%SS%3N)
 
     command mv --verbose --backup=simple --suffix=$(date -u +.%Y%m%dT%H%M%SS%3N) --target-directory "$TRASH_DIR" $argv
