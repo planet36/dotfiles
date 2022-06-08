8af57f587e610f4d2df2b70382bd43a923faf09f
planet36 <planet36@users.noreply.github.com>
2022-06-08 12:20:03 -0500

    Add bidict.py

---
+++ link/.local/lib/python/bidict.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+'''Bidirectional dictionary
+Adapted from
+https://stackoverflow.com/a/21894086'''
+
+# pylint: disable=missing-class-docstring
+class bidict(dict):
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.inverse = {}
+		for key, value in self.items():
+			self.inverse.setdefault(value, set()).add(key)
+
+	def __setitem__(self, key, value):
+		if key in self and self[key] in self.inverse:
+			self.inverse[self[key]].remove(key)
+			# If the last value was removed
+			if not self.inverse[self[key]]: # empty
+				del self.inverse[self[key]]
+		super().__setitem__(key, value)
+		self.inverse.setdefault(value, set()).add(key)
+
+	def __delitem__(self, key):
+		if key in self and self[key] in self.inverse:
+			self.inverse[self[key]].remove(key)
+			# If the last value was removed
+			if not self.inverse[self[key]]: # empty
+				del self.inverse[self[key]]
+		super().__delitem__(key)
