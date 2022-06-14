916d9a312696095dca20858c2e15619eafa4e54f
planet36 <planet36@users.noreply.github.com>
2022-06-14 15:30:29 -0500

    Fix brief description

--- link/.local/include/uint_set_bits.h
+++ link/.local/include/uint_set_bits.h
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Steven Ward
 // SPDX-License-Identifier: OSL-3.0
 
-/// Macro to create bitmask with 1 bit set
+/// Macro to create low-pass bitmask
 /**
 \file
 \author Steven Ward
