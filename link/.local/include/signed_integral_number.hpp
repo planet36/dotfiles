df5645a7350fb4d4a6e8de53bfeb64bc43efb393
planet36 <planet36@users.noreply.github.com>
2022-06-16 09:49:00 -0500

    Add signed_integral_number.hpp, unsigned_integral_number.hpp

---
+++ link/.local/include/signed_integral_number.hpp
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Concept for a signed integral number type
+/**
+\file
+\author Steven Ward
+\sa https://en.cppreference.com/w/cpp/language/type
+*/
+
+#pragma once
+
+#include "type_any_of.hpp"
+
+#include <type_traits>
+
+// Character types and bool are excluded
+template <typename T>
+struct is_signed_integral_number: is_type_any_of<std::remove_cv_t<T>,
+	short,
+	int,
+	long,
+	long long>
+{};
+
+template <typename T>
+inline constexpr bool is_signed_integral_number_v = is_signed_integral_number<T>::value;
+
+template <typename T>
+concept signed_integral_number = is_signed_integral_number_v<T>;
---
+++ link/.local/include/unsigned_integral_number.hpp
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Concept for an unsigned integral number type
+/**
+\file
+\author Steven Ward
+\sa https://en.cppreference.com/w/cpp/language/type
+*/
+
+#pragma once
+
+#include "type_any_of.hpp"
+
+#include <type_traits>
+
+// Character types and bool are excluded
+template <typename T>
+struct is_unsigned_integral_number: is_type_any_of<std::remove_cv_t<T>,
+	unsigned short,
+	unsigned int,
+	unsigned long,
+	unsigned long long>
+{};
+
+template <typename T>
+inline constexpr bool is_unsigned_integral_number_v = is_unsigned_integral_number<T>::value;
+
+template <typename T>
+concept unsigned_integral_number = is_unsigned_integral_number_v<T>;
