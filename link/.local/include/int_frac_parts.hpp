5497328e5f3288122442e93201bfe1d433058017
planet36 <planet36@users.noreply.github.com>
2022-07-05 09:27:20 -0500

    Add int_frac_parts.hpp

---
+++ link/.local/include/int_frac_parts.hpp
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Get the integer part and fractional part of a floating-point number
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <cmath>
+#include <concepts>
+
+/// get the integer part of a floating-point number
+template <std::floating_point T>
+constexpr T
+int_part(const T x)
+{
+	T i{};
+	(void) std::modf(x, &i);
+	return i;
+}
+
+/// get the fractional part of a floating-pointing point number
+template <std::floating_point T>
+constexpr T
+frac_part(const T x)
+{
+	T i{};
+	return std::modf(x, &i);
+}
+
+/// get the integer and fractional parts of a floating-point number
+template <std::floating_point T>
+constexpr void
+int_frac_parts(const T x, T& i, T& f)
+{
+	f = std::modf(x, &i);
+}
