3d453f4b053c201700dc43fac20fd12d3b37d0e8
planet36 <planet36@users.noreply.github.com>
2022-07-08 08:59:26 -0500

    Replace to_integral with to_integral_value

--- link/.local/lib/python/remove_exponent.py
+++ link/.local/lib/python/remove_exponent.py
@@ -9,4 +9,4 @@ from decimal import Decimal
 # copied from
 # https://docs.python.org/3/library/decimal.html#decimal-faq
 def remove_exponent(d: Decimal):
-	return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
+	return d.quantize(Decimal(1)) if d == d.to_integral_value() else d.normalize()
