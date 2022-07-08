8317a5c72eb0563410fce1280efd52a90529e8dd
planet36 <planet36@users.noreply.github.com>
2022-07-08 09:02:39 -0500

    Add return type to remove_exponent

--- link/.local/lib/python/remove_exponent.py
+++ link/.local/lib/python/remove_exponent.py
@@ -8,5 +8,5 @@ from decimal import Decimal
 
 # copied from
 # https://docs.python.org/3/library/decimal.html#decimal-faq
-def remove_exponent(d: Decimal):
+def remove_exponent(d: Decimal) -> Decimal:
 	return d.quantize(Decimal(1)) if d == d.to_integral_value() else d.normalize()
