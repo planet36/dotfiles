e1d219e7a29b3ce40b617ca30b85fec45b692dc4
planet36 <planet36@users.noreply.github.com>
2022-07-15 09:19:59 -0500

    Use the reentrant code if DEBUG is defined
    Otherwise, use the original, simple code.

--- link/.local/include/current_year.h
+++ link/.local/include/current_year.h
@@ -5,42 +5,51 @@
 /**
 \file
 \author Steven Ward
+\sa https://en.cppreference.com/w/c/chrono/time
+\sa https://en.cppreference.com/w/c/chrono/localtime
+\sa https://en.cppreference.com/w/c/chrono/gmtime
 */
 
 #pragma once
 
+#ifdef DEBUG
 #include <limits.h>
 #include <stdio.h>
+#endif
 #include <time.h>
 
 static int
 current_year_local()
 {
-	// https://en.cppreference.com/w/c/chrono/time
 	const time_t now_time_t = time(NULL);
+#ifdef DEBUG
 	struct tm now_tm = {0};
 	// https://pubs.opengroup.org/onlinepubs/9699919799/functions/tzset.html
 	tzset();
-	// https://en.cppreference.com/w/c/chrono/localtime
 	if (localtime_r(&now_time_t, &now_tm) == NULL)
 	{
 		perror("localtime_r");
 		return INT_MIN;
 	}
 	return now_tm.tm_year + 1900;
+#else
+	return localtime(&now_time_t)->tm_year + 1900;
+#endif
 }
 
 static int
 current_year_utc()
 {
-	// https://en.cppreference.com/w/c/chrono/time
 	const time_t now_time_t = time(NULL);
+#ifdef DEBUG
 	struct tm now_tm = {0};
-	// https://en.cppreference.com/w/c/chrono/gmtime
 	if (gmtime_r(&now_time_t, &now_tm) == NULL)
 	{
 		perror("gmtime_r");
 		return INT_MIN;
 	}
 	return now_tm.tm_year + 1900;
+#else
+	return gmtime(&now_time_t)->tm_year + 1900;
+#endif
 }
--- link/.local/include/current_year.hpp
+++ link/.local/include/current_year.hpp
@@ -5,12 +5,19 @@
 /**
 \file
 \author Steven Ward
+\sa https://en.cppreference.com/w/cpp/chrono/c/time
+\sa https://en.cppreference.com/w/c/chrono/localtime
+\sa https://en.cppreference.com/w/cpp/chrono/c/localtime
+\sa https://en.cppreference.com/w/cpp/chrono/c/gmtime
+\sa https://en.cppreference.com/w/c/chrono/gmtime
 */
 
 #pragma once
 
 #include <ctime>
+#ifdef DEBUG
 #include <system_error>
+#endif
 
 namespace
 {
@@ -18,35 +25,39 @@ namespace
 int
 current_year_local()
 {
-	// https://en.cppreference.com/w/cpp/chrono/c/time
 	const std::time_t now_time_t = std::time(nullptr);
+#ifdef DEBUG
 	tm now_tm{};
 	// https://pubs.opengroup.org/onlinepubs/9699919799/functions/tzset.html
 	::tzset();
 	// NOTE: localtime_r is not in std::
-	// https://en.cppreference.com/w/c/chrono/localtime
 	if (::localtime_r(&now_time_t, &now_tm) == nullptr)
 	{
 		throw std::system_error(std::make_error_code(std::errc(errno)),
 		                        "localtime_r");
 	}
 	return now_tm.tm_year + 1900;
+#else
+	return std::localtime(&now_time_t)->tm_year + 1900;
+#endif
 }
 
 int
 current_year_utc()
 {
-	// https://en.cppreference.com/w/cpp/chrono/c/time
 	const std::time_t now_time_t = std::time(nullptr);
+#ifdef DEBUG
 	tm now_tm{};
 	// NOTE: gmtime_r is not in std::
-	// https://en.cppreference.com/w/c/chrono/gmtime
 	if (::gmtime_r(&now_time_t, &now_tm) == nullptr)
 	{
 		throw std::system_error(std::make_error_code(std::errc(errno)),
 		                        "gmtime_r");
 	}
 	return now_tm.tm_year + 1900;
+#else
+	return std::gmtime(&now_time_t)->tm_year + 1900;
+#endif
 }
 
 }
