9469c92bf4c8646ebb9e7c1ab8a93577ad1e76c2
planet36 <planet36@users.noreply.github.com>
2022-08-04 20:27:10 -0500

    Move std::array versions of fill_rand below the plain integer ones

--- link/.local/include/fill_rand.hpp
+++ link/.local/include/fill_rand.hpp
@@ -21,18 +21,18 @@
 // arc4random_buf
 // https://www.gnu.org/software/libc/manual/html_node/High-Quality-Random.html
 
-template <std::unsigned_integral T, size_t N>
-void
-fill_rand(std::array<T, N>& arr)
-{
-	arc4random_buf(arr.data(), sizeof(T) * N);
-}
-
 template <std::unsigned_integral T>
 void
 fill_rand(T& x)
 {
 	arc4random_buf(&x, sizeof(T));
+}
+
+template <std::unsigned_integral T, size_t N>
+void
+fill_rand(std::array<T, N>& arr)
+{
+	arc4random_buf(arr.data(), sizeof(T) * N);
 }
 
 #elif defined(_GLIBCXX_HAVE_GETENTROPY)
@@ -43,18 +43,6 @@ fill_rand(T& x)
 // getentropy
 // https://www.gnu.org/software/libc/manual/html_node/Unpredictable-Bytes.html
 // Max num bytes allowed is 256
-
-template <std::unsigned_integral T, size_t N>
-requires (sizeof(T) * N <= 256)
-void
-fill_rand(std::array<T, N>& arr)
-{
-	if (getentropy(arr.data(), sizeof(T) * N) < 0)
-	{
-		throw std::system_error(std::make_error_code(std::errc(errno)),
-		                        "getentropy");
-	}
-}
 
 template <std::unsigned_integral T>
 requires (sizeof(T) <= 256)
@@ -68,10 +56,38 @@ fill_rand(T& x)
 	}
 }
 
+template <std::unsigned_integral T, size_t N>
+requires (sizeof(T) * N <= 256)
+void
+fill_rand(std::array<T, N>& arr)
+{
+	if (getentropy(arr.data(), sizeof(T) * N) < 0)
+	{
+		throw std::system_error(std::make_error_code(std::errc(errno)),
+		                        "getentropy");
+	}
+}
+
 #else
 
 #include <random>
 #include <utility>
+
+template <std::unsigned_integral T>
+void
+fill_rand(T& x)
+{
+	static std::random_device rd;
+
+	// std::random_device::result_type is unsigned int
+	// https://en.cppreference.com/w/cpp/numeric/random/random_device
+	if constexpr (sizeof(T) <= sizeof(typename std::random_device::result_type))
+		x = rd();
+	else if constexpr (sizeof(T) <= 2*sizeof(typename std::random_device::result_type))
+		x = int_join(rd(), rd());
+	else
+		std::unreachable();
+}
 
 template <std::unsigned_integral T, size_t N>
 void
@@ -90,22 +106,6 @@ fill_rand(std::array<T, N>& arr)
 		else
 			std::unreachable();
 	}
-}
-
-template <std::unsigned_integral T>
-void
-fill_rand(T& x)
-{
-	static std::random_device rd;
-
-	// std::random_device::result_type is unsigned int
-	// https://en.cppreference.com/w/cpp/numeric/random/random_device
-	if constexpr (sizeof(T) <= sizeof(typename std::random_device::result_type))
-		x = rd();
-	else if constexpr (sizeof(T) <= 2*sizeof(typename std::random_device::result_type))
-		x = int_join(rd(), rd());
-	else
-		std::unreachable();
 }
 
 #endif
