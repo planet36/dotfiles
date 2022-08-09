40b7016aa29b8911b58fe95fdeeee03bee14f280
planet36 <planet36@users.noreply.github.com>
2022-08-09 15:11:51 -0500

    Fail early if 128-bit int doesn't exist
    Improve comments and add links.

--- link/.local/include/mum_mix.hpp
+++ link/.local/include/mum_mix.hpp
@@ -6,15 +6,19 @@
 \file
 \author Steven Ward
 \sa https://github.com/vnmakarov/mum-hash
+\sa https://github.com/vnmakarov/mum-hash/blob/master/mum.h#L104
+\sa https://github.com/vnmakarov/mum-hash/blob/master/mum-prng.h#L74
 */
 
 #pragma once
 
-#if defined(__SIZEOF_INT128__)
+#if !defined(__SIZEOF_INT128__)
+#error "__SIZEOF_INT128__ not defined"
+#endif
 
 #include "int_parts_union.hpp"
 
-/// Multiply 64-bit V and P and return sum of high and low parts of the result.
+/// Multiply 64-bit \a V and \a P and return sum of high and low parts of the result.
 constexpr uint64_t
 mum_mix_add(const uint64_t v, const uint64_t p)
 {
@@ -25,16 +29,10 @@ mum_mix_add(const uint64_t v, const uint64_t p)
 	return r.parts[1] + r.parts[0];
 }
 
-/// Multiply 64-bit V and P and return xor of high and low parts of the result.
+/// Multiply 64-bit \a V and \a P and return xor of high and low parts of the result.
 constexpr uint64_t
 mum_mix_xor(const uint64_t v, const uint64_t p)
 {
 	const int_parts<uint64_t> r{.whole = __uint128_t{v} * p};
 	return r.parts[1] ^ r.parts[0];
 }
-
-#else
-
-#error "__SIZEOF_INT128__ not defined"
-
-#endif
