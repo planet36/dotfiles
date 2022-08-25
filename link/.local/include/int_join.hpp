b60c6cab89843d90daa70c03f6b5962f3be74f74
planet36 <planet36@users.noreply.github.com>
2022-08-25 12:24:02 -0500

    Use bit shift and bitwise OR instead of a union
    Upon initializing the int_parts union, "parts" is the active member.
    It's undefined behavior to access "whole".  UB is not allowed in a
    constexpr expression.

--- link/.local/include/int_join.hpp
+++ link/.local/include/int_join.hpp
@@ -9,19 +9,14 @@
 
 #pragma once
 
-#include "int_parts_union.hpp"
+#include "int_bytes.hpp"
 
-#include <bit>
 #include <concepts>
+#include <limits>
 
 template <std::unsigned_integral T>
 constexpr auto
 int_join(const T hi, const T lo)
 {
-	if constexpr (std::endian::native == std::endian::little)
-		return int_parts<T>{.parts = {lo, hi}}.whole;
-	else if constexpr (std::endian::native == std::endian::big)
-		return int_parts<T>{.parts = {hi, lo}}.whole;
-	else // mixed endian
-		__builtin_unreachable();
+	return (static_cast<next_larger<T>>(hi) << std::numeric_limits<T>::digits) | lo;
 }
