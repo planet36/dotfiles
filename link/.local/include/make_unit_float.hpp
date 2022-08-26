ce22a9aa6cd11a2276b82267096564f332d42da3
planet36 <planet36@users.noreply.github.com>
2022-08-26 07:33:55 -0500

    Rename template parameter

--- link/.local/include/make_unit_float.hpp
+++ link/.local/include/make_unit_float.hpp
@@ -20,31 +20,31 @@ Inspired by <https://prng.di.unimi.it/>
 #include <concepts>
 #include <limits>
 
-template <std::unsigned_integral T>
-requires (sizeof(T) == 4 || sizeof(T) == 8)
-constexpr auto make_unit_float(const T x)
+template <std::unsigned_integral Ti>
+requires (sizeof(Ti) == 4 || sizeof(Ti) == 8)
+constexpr auto make_unit_float(const Ti x)
 {
-	using f_t = float_bytes<sizeof(x)>;
-	static_assert(sizeof(f_t) == sizeof(T));
+	using Tf = float_bytes<sizeof(x)>;
+	static_assert(sizeof(Tf) == sizeof(Ti));
 
 	// float: 23
 	// double: 52
-	constexpr auto shift_l = std::numeric_limits<f_t>::digits - 1;
+	constexpr auto shift_l = std::numeric_limits<Tf>::digits - 1;
 
-	static_assert(std::numeric_limits<T>::digits >= shift_l);
+	static_assert(std::numeric_limits<Ti>::digits >= shift_l);
 
 	// float: 32-23 = 9
 	// double: 64-52 = 12
-	constexpr auto shift_r = std::numeric_limits<T>::digits - shift_l;
+	constexpr auto shift_r = std::numeric_limits<Ti>::digits - shift_l;
 
 	// float: UINT32_C(0x7F)
 	// double: UINT64_C(0x3FF)
-	[[maybe_unused]] constexpr auto sign_exp_bits = T{std::numeric_limits<f_t>::max_exponent - 1};
+	[[maybe_unused]] constexpr auto sign_exp_bits = Ti{std::numeric_limits<Tf>::max_exponent - 1};
 
 	if constexpr (__BYTE_ORDER__ == __FLOAT_WORD_ORDER__)
 		return bits_to_float(sign_exp_bits << shift_l | x >> shift_r) - 1;
 	else
-		return (x >> shift_r) / f_t{T{1} << shift_l};
+		return (x >> shift_r) / Tf{Ti{1} << shift_l};
 }
 #else
 constexpr float
