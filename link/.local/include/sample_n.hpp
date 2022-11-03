b5c7e04ee70043384f85d28eda2813b4ebb446ef
planet36 <planet36@users.noreply.github.com>
2022-11-03 14:43:11 -0500

    Add sample_n.hpp

---
+++ link/.local/include/sample_n.hpp
@@ -0,0 +1,70 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Get a sample of N elements from a \c string or \c vector
+/**
+\file
+\author Steven Ward
+\c sample_n works on types that satisfy both \c AllocatorAwareContainer and \c ContiguousContainer.
+*/
+
+#pragma once
+
+#include <random>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+template <std::uniform_random_bit_generator G>
+auto sample_n(const std::string& in, const size_t num_samples, G& gen)
+{
+	std::remove_cvref_t<decltype(in)> out;
+
+	if (!in.empty())
+	{
+		out.reserve(num_samples);
+
+		// https://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution/uniform_int_distribution
+		std::uniform_int_distribution<size_t> dis(0, in.size()-1);
+
+		for (size_t n = 0; n < num_samples; ++n)
+		{
+			out.push_back(in[dis(gen)]);
+		}
+	}
+
+	return out;
+}
+
+template <std::uniform_random_bit_generator G>
+auto sample_n(const std::string& in, const size_t num_samples, G&& gen)
+{
+	return sample_n(in, num_samples, gen);
+}
+
+template <typename T, std::uniform_random_bit_generator G>
+auto sample_n(const std::vector<T>& in, const size_t num_samples, G& gen)
+{
+	std::remove_cvref_t<decltype(in)> out;
+
+	if (!in.empty())
+	{
+		out.reserve(num_samples);
+
+		// https://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution/uniform_int_distribution
+		std::uniform_int_distribution<size_t> dis(0, in.size()-1);
+
+		for (size_t n = 0; n < num_samples; ++n)
+		{
+			out.push_back(in[dis(gen)]);
+		}
+	}
+
+	return out;
+}
+
+template <typename T, std::uniform_random_bit_generator G>
+auto sample_n(const std::vector<T>& in, const size_t num_samples, G&& gen)
+{
+	return sample_n(in, num_samples, gen);
+}
