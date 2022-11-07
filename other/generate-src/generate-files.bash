f924fc4ab1f9a999ae0c6cdd1f225044e49eb7fe
planet36 <planet36@users.noreply.github.com>
2022-11-07 09:52:27 -0500

    Add generate-ascii.py

--- other/generate-src/generate-files.bash
+++ other/generate-src/generate-files.bash
@@ -13,6 +13,7 @@ OUT_DIR="$(git rev-parse --show-toplevel)"/link/.local/include
 
 calc -d -m 4 -f generate-series-approx-coeff-aux-lat.cal > "$OUT_DIR"/aux-lat-conv.hpp
 calc -d -m 4 -f generate-series-approx-coeff-tm.cal      > "$OUT_DIR"/utm-ups-const.hpp
+python3 generate-ascii.py                                > "$OUT_DIR"/ascii.hpp
 python3 generate-gcc-machine-modes-typedefs.py           > "$OUT_DIR"/gcc-machine-modes-typedefs.hpp
 python3 generate-math-const.py                           > "$OUT_DIR"/math-const.hpp
 
@@ -20,8 +21,8 @@ cat <<EOT
 # Run these commands:
 
 git diff \
-$OUT_DIR/{aux-lat-conv.hpp,gcc-machine-modes-typedefs.hpp,math-const.hpp,utm-ups-const.hpp}
+$OUT_DIR/{ascii.hpp,aux-lat-conv.hpp,gcc-machine-modes-typedefs.hpp,math-const.hpp,utm-ups-const.hpp}
 
 git commit -m 'Update generated source code files' \
-$OUT_DIR/{aux-lat-conv.hpp,gcc-machine-modes-typedefs.hpp,math-const.hpp,utm-ups-const.hpp}
+$OUT_DIR/{ascii.hpp,aux-lat-conv.hpp,gcc-machine-modes-typedefs.hpp,math-const.hpp,utm-ups-const.hpp}
 EOT
