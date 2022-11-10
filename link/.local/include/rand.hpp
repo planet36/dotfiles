ed221fcf925e6afebcc4b2023cdf0150aa18b8fa
planet36 <planet36@users.noreply.github.com>
2022-11-10 09:46:16 -0500

    Add overloads of functions to take a std::uniform_random_bit_generator

--- link/.local/include/rand.hpp
+++ link/.local/include/rand.hpp
@@ -72,25 +72,53 @@ reseed(const G::seed_bytes_type& bytes)
 \sa https://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution
 \return a uniformly distributed random integer within the interval [\a a, \a b]
 */
+template <integral_number T, std::uniform_random_bit_generator G>
+T
+rand_int(const T a, const T b, G& gen)
+{
+	static_assert(std::numeric_limits<typename G::result_type>::digits >=
+	              std::numeric_limits<T>::digits);
+	return std::uniform_int_distribution<T>{a, b}(gen);
+}
+
+template <integral_number T, std::uniform_random_bit_generator G>
+T
+rand_int(const T a, const T b, G&& gen)
+{
+	return rand_int(a, b, gen);
+}
+
 template <integral_number T>
 T
 rand_int(const T a, const T b)
 {
-	static_assert(std::numeric_limits<G::result_type>::digits >=
-	              std::numeric_limits<T>::digits);
-	return std::uniform_int_distribution<T>{a, b}(instance());
+	return rand_int(a, b, instance());
 }
 
 /**
 \return a uniformly distributed random integer within the interval [min, max]
 */
+template <integral_number T, std::uniform_random_bit_generator G>
+T
+rand_int(G& gen)
+{
+	static_assert(std::numeric_limits<typename G::result_type>::digits >=
+	              std::numeric_limits<T>::digits);
+	return gen();
+}
+
+template <integral_number T, std::uniform_random_bit_generator G>
+T
+rand_int(G&& gen)
+{
+	return rand_int<T>(gen);
+}
+
 template <integral_number T>
 T
 rand_int()
 {
-	static_assert(std::numeric_limits<G::result_type>::digits >=
-	              std::numeric_limits<T>::digits);
-	return instance().next();
+	return rand_int<T>(instance());
 }
 
 /**
@@ -98,27 +126,27 @@ rand_int()
 \sa https://arxiv.org/abs/1805.10941
 \return a uniformly distributed random unsigned integer within the interval [0, \a s)
 */
-template <std::unsigned_integral T>
+template <std::unsigned_integral T, std::uniform_random_bit_generator G>
 T
-rand_uint_half_open(const T s)
+rand_uint_half_open(const T s, G& gen)
 {
 	if (s == 1)
 		return 0;
 
 	if (s == 0)
-		return rand_int<T>();
+		return rand_int<T>(gen);
 
 	using T2 = next_larger<T>;
 	T2 m;
 
-	m = rand_int<T>(); // x in [0, 2^L)
+	m = rand_int<T>(gen); // x in [0, 2^L)
 	m *= s; // x in [0, s * 2^L)
 	if (static_cast<T>(m) < s)
 	{
 		const T min = std::numeric_limits<T>::max() % s; // 2^L mod s
 		while (static_cast<T>(m) < min)
 		{
-			m = rand_int<T>(); // x in [0, 2^L)
+			m = rand_int<T>(gen); // x in [0, 2^L)
 			m *= s; // x in [0, s * 2^L)
 		}
 	}
@@ -126,137 +154,249 @@ rand_uint_half_open(const T s)
 	return m >> std::numeric_limits<T>::digits; // in [0, s)
 }
 
+template <std::unsigned_integral T, std::uniform_random_bit_generator G>
+T
+rand_uint_half_open(const T s, G&& gen)
+{
+	return rand_uint_half_open(s, gen);
+}
+
+template <std::unsigned_integral T>
+T
+rand_uint_half_open(const T s)
+{
+	return rand_uint_half_open(s, instance());
+}
+
 /**
 \return a uniformly distributed random floating-point number within the interval [0, 1)
 */
+template <std::uniform_random_bit_generator G>
 float
-rand_float()
+rand_float(G& gen)
 {
 	using T = float;
 
 #if 1
-	static_assert(std::numeric_limits<G::result_type>::digits >=
+	static_assert(std::numeric_limits<typename G::result_type>::digits >=
 	              std::numeric_limits<T>::digits,
 	              "PRNG does not provide sufficient bits");
-	return make_unit_float(instance().next());
+	return make_unit_float(gen());
 #else
 	if constexpr (std::numeric_limits<T>::digits <=
-	              std::numeric_limits<G::result_type>::digits)
+	              std::numeric_limits<typename G::result_type>::digits)
 	{
-		return make_unit_float(instance().next());
+		return make_unit_float(gen());
 	}
 	else if constexpr (std::numeric_limits<T>::digits <=
-	                   2 * std::numeric_limits<G::result_type>::digits)
+	                   2 * std::numeric_limits<typename G::result_type>::digits)
 	{
-		return make_unit_float(int_join(instance().next(),
-		                                instance().next()));
+		return make_unit_float(int_join(gen(),
+		                                gen()));
 	}
 #endif
+}
+
+template <std::uniform_random_bit_generator G>
+float
+rand_float(G&& gen)
+{
+	return rand_float(gen);
+}
+
+float
+rand_float()
+{
+	return rand_float(instance());
 }
 
 /**
 \return a uniformly distributed random floating-point number within the interval [0, 1)
 */
+template <std::uniform_random_bit_generator G>
 double
-rand_double()
+rand_double(G& gen)
 {
 	using T = double;
 
 #if 1
-	static_assert(std::numeric_limits<G::result_type>::digits >=
+	static_assert(std::numeric_limits<typename G::result_type>::digits >=
 	              std::numeric_limits<T>::digits,
 	              "PRNG does not provide sufficient bits");
-	return make_unit_double(instance().next());
+	return make_unit_double(gen());
 #else
 	if constexpr (std::numeric_limits<T>::digits <=
-	              std::numeric_limits<G::result_type>::digits)
+	              std::numeric_limits<typename G::result_type>::digits)
 	{
-		return make_unit_double(instance().next());
+		return make_unit_double(gen());
 	}
 	else if constexpr (std::numeric_limits<T>::digits <=
-	                   2 * std::numeric_limits<G::result_type>::digits)
+	                   2 * std::numeric_limits<typename G::result_type>::digits)
 	{
-		return make_unit_double(int_join(instance().next(),
-		                                 instance().next()));
+		return make_unit_double(int_join(gen(),
+		                                 gen()));
 	}
 #endif
+}
+
+template <std::uniform_random_bit_generator G>
+double
+rand_double(G&& gen)
+{
+	return rand_double(gen);
+}
+
+double
+rand_double()
+{
+	return rand_double(instance());
 }
 
 /**
 \return a uniformly distributed random floating-point number within the interval [0, 1)
 */
+template <std::uniform_random_bit_generator G>
 long double
-rand_long_double()
+rand_long_double(G& gen)
 {
 	using T = long double;
 
 #if 1
-	static_assert(std::numeric_limits<G::result_type>::digits >=
+	static_assert(std::numeric_limits<typename G::result_type>::digits >=
 	              std::numeric_limits<T>::digits,
 	              "PRNG does not provide sufficient bits");
-	return make_unit_long_double(instance().next());
+	return make_unit_long_double(gen());
 #else
 	if constexpr (std::numeric_limits<T>::digits <=
-	              std::numeric_limits<G::result_type>::digits)
+	              std::numeric_limits<typename G::result_type>::digits)
 	{
-		return make_unit_long_double(instance().next());
+		return make_unit_long_double(gen());
 	}
 	else if constexpr (std::numeric_limits<T>::digits <=
-	                   2 * std::numeric_limits<G::result_type>::digits)
+	                   2 * std::numeric_limits<typename G::result_type>::digits)
 	{
-		return make_unit_long_double(int_join(instance().next(),
-		                                      instance().next()));
+		return make_unit_long_double(int_join(gen(),
+		                                      gen()));
 	}
 #endif
 }
 
+template <std::uniform_random_bit_generator G>
+long double
+rand_long_double(G&& gen)
+{
+	return rand_long_double(gen);
+}
+
+long double
+rand_long_double()
+{
+	return rand_long_double(instance());
+}
+
 /**
 \pre \a a < \a b
 \return a uniformly distributed random floating-point number within the interval [\a a, \a b)
 */
+template <std::uniform_random_bit_generator G>
+float
+rand_float(const float a, const float b, G& gen)
+{
+	return rand_float(gen) * (b - a) + a;
+}
+
+template <std::uniform_random_bit_generator G>
+float
+rand_float(const float a, const float b, G&& gen)
+{
+	return rand_float(a, b, gen);
+}
+
 float
 rand_float(const float a, const float b)
 {
-	return rand_float() * (b - a) + a;
+	return rand_float(a, b, instance());
 }
 
 /**
 \pre \a a < \a b
 \return a uniformly distributed random floating-point number within the interval [\a a, \a b)
 */
+template <std::uniform_random_bit_generator G>
+double
+rand_double(const double a, const double b, G& gen)
+{
+	return rand_double(gen) * (b - a) + a;
+}
+
+template <std::uniform_random_bit_generator G>
+double
+rand_double(const double a, const double b, G&& gen)
+{
+	return rand_double(a, b, gen);
+}
+
 double
 rand_double(const double a, const double b)
 {
-	return rand_double() * (b - a) + a;
+	return rand_double(a, b, instance());
 }
 
 /**
 \pre \a a < \a b
 \return a uniformly distributed random floating-point number within the interval [\a a, \a b)
 */
+template <std::uniform_random_bit_generator G>
+long double
+rand_long_double(const long double a, const long double b, G& gen)
+{
+	return rand_long_double(gen) * (b - a) + a;
+}
+
+template <std::uniform_random_bit_generator G>
+long double
+rand_long_double(const long double a, const long double b, G&& gen)
+{
+	return rand_long_double(a, b, gen);
+}
+
 long double
 rand_long_double(const long double a, const long double b)
 {
-	return rand_long_double() * (b - a) + a;
+	return rand_long_double(a, b, instance());
 }
 
 /**
 * \sa https://martin.ankerl.com/2018/12/08/fast-random-bool/
 * \sa https://gist.github.com/martinus/c43d99ad0008e11fcdbf06982e25f464
 */
+template <std::uniform_random_bit_generator G>
 bool
-rand_bool()
+rand_bool(G& gen)
 {
-	using T = G::result_type;
+	using T = typename G::result_type;
 	static constexpr T mask_one_msb = T{1} << (std::numeric_limits<T>::digits-1);
 	static thread_local T x = 1;
 
 	if (x == 1) [[unlikely]]
 	{
-		x = instance().next() | mask_one_msb;
+		x = gen() | mask_one_msb;
 	}
 
 	const bool ret = x & 1;
 	x >>= 1;
 	return ret;
+}
+
+template <std::uniform_random_bit_generator G>
+bool
+rand_bool(G&& gen)
+{
+	return rand_bool(gen);
+}
+
+bool
+rand_bool()
+{
+	return rand_bool(instance());
 }
