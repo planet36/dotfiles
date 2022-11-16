5b2353ffc6de7ee299d5ff328f5301b813f8887e
planet36 <planet36@users.noreply.github.com>
2022-11-15 22:49:13 -0500

    Remove default argument from strip() functions
    Instead, add overloads and pass ascii_whitespace_s as the delim_set.

--- link/.local/include/strip.hpp
+++ link/.local/include/strip.hpp
@@ -18,39 +18,73 @@ The \c trim() functions use <code><algorithm></code> functions to find delimiter
 
 #include <string>
 
-void rstrip(std::string& s, const std::string& delim_set = ascii_whitespace_s)
+void rstrip(std::string& s, const std::string& delim_set)
 {
 	const auto i = s.find_last_not_of(delim_set);
 	(void)s.erase(i+1);
 	//s.resize(i+1);
 }
 
-void lstrip(std::string& s, const std::string& delim_set = ascii_whitespace_s)
+void lstrip(std::string& s, const std::string& delim_set)
 {
 	const auto i = s.find_first_not_of(delim_set);
 	(void)s.erase(0, i);
 }
 
-void strip(std::string& s, const std::string& delim_set = ascii_whitespace_s)
+void strip(std::string& s, const std::string& delim_set)
 {
 	rstrip(s, delim_set);
 	lstrip(s, delim_set);
 }
 
-std::string rstrip_copy(std::string s, const std::string& delim_set = ascii_whitespace_s)
+std::string rstrip_copy(std::string s, const std::string& delim_set)
 {
 	rstrip(s, delim_set);
 	return s;
 }
 
-std::string lstrip_copy(std::string s, const std::string& delim_set = ascii_whitespace_s)
+std::string lstrip_copy(std::string s, const std::string& delim_set)
 {
 	lstrip(s, delim_set);
 	return s;
 }
 
-std::string strip_copy(std::string s, const std::string& delim_set = ascii_whitespace_s)
+std::string strip_copy(std::string s, const std::string& delim_set)
 {
 	strip(s, delim_set);
+	return s;
+}
+
+void rstrip(std::string& s)
+{
+	rstrip(s, ascii_whitespace_s);
+}
+
+void lstrip(std::string& s)
+{
+	lstrip(s, ascii_whitespace_s);
+}
+
+void strip(std::string& s)
+{
+	rstrip(s);
+	lstrip(s);
+}
+
+std::string rstrip_copy(std::string s)
+{
+	rstrip(s);
+	return s;
+}
+
+std::string lstrip_copy(std::string s)
+{
+	lstrip(s);
+	return s;
+}
+
+std::string strip_copy(std::string s)
+{
+	strip(s);
 	return s;
 }
