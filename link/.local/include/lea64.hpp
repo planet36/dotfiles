a5ff3a4c5baf5a52b97797f8e0595692eb3971f2
planet36 <planet36@users.noreply.github.com>
2022-11-23 12:03:21 -0500

    Add lea64.hpp, moremur.hpp

---
+++ link/.local/include/lea64.hpp
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// lea64 PRNG
+/**
+\file
+\author Steven Ward
+\sa https://mostlymangling.blogspot.com/2021/11/applying-rrc-64-test-to-degski64-and.html
+*/
+
+#pragma once
+
+#include "def_urbg_class_details.hpp"
+#include "gamma.hpp"
+
+struct lea64
+{
+	using state_type = uint64_t;
+	using result_type = uint64_t;
+
+DEF_URBG_CLASS_DETAILS(lea64)
+
+	result_type next()
+	{
+		static constexpr uint64_t inc = gamma64;
+		static_assert(inc & 1, "must be odd");
+
+		static constexpr uint64_t M1 = 0xdaba0b6eb09322e3; // not prime (popcount = 32)
+		static constexpr uint64_t M2 = 0xdaba0b6eb09322e3; // not prime (popcount = 32)
+		static constexpr unsigned int S1 = 32;
+		static constexpr unsigned int S2 = 32;
+		static constexpr unsigned int S3 = 32;
+		static_assert(M1 & 1, "must be odd");
+		static_assert(M2 & 1, "must be odd");
+
+		auto x = (s += inc);
+
+		x ^= x >> S1;
+		x *= M1;
+		x ^= x >> S2;
+		x *= M2;
+		x ^= x >> S3;
+		return x;
+	}
+};
