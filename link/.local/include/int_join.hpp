e0fec94d78d61ebbfbb755cf79759d3f483d0e9f
planet36 <planet36@users.noreply.github.com>
2022-12-02 12:16:34 -0500

    Put bit shift and bitwise OR code in an "if consteval" block
    The union code cannot be used in constexpr expression, but it has one
    less mov instruction according to testing on <https://godbolt.org/>.

--- link/.local/include/int_join.hpp
+++ link/.local/include/int_join.hpp
@@ -10,7 +10,9 @@
 #pragma once
 
 #include "int_bytes.hpp"
+#include "int_parts_union.hpp"
 
+#include <bit>
 #include <concepts>
 #include <limits>
 
@@ -18,6 +20,19 @@ template <std::unsigned_integral T>
 constexpr auto
 int_join(const T hi, const T lo)
 {
-	using T2 = next_larger<T>;
-	return (static_cast<T2>(hi) << std::numeric_limits<T>::digits) | lo;
+	// https://en.cppreference.com/w/cpp/language/if#Consteval_if
+	if consteval
+	{
+		using T2 = next_larger<T>;
+		return (static_cast<T2>(hi) << std::numeric_limits<T>::digits) | lo;
+	}
+	else
+	{
+		if constexpr (std::endian::native == std::endian::little)
+			return int_parts<T>{.parts = {lo, hi}}.whole;
+		else if constexpr (std::endian::native == std::endian::big)
+			return int_parts<T>{.parts = {hi, lo}}.whole;
+		else // mixed endian
+			__builtin_unreachable();
+	}
 }
