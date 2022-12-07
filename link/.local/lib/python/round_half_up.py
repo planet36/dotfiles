0202b819c75dba88c4e26e6c362f90b60ae19ad5
planet36 <planet36@users.noreply.github.com>
2022-12-07 17:48:54 -0500

    From decimal, only import Decimal (as D) and ROUND_HALF_UP

--- link/.local/lib/python/round_half_up.py
+++ link/.local/lib/python/round_half_up.py
@@ -8,13 +8,12 @@
 __author__ = 'Steven Ward'
 __license__ = 'OSL-3.0'
 
-import decimal
+from decimal import Decimal as D, ROUND_HALF_UP
 
 # https://docs.python.org/3/library/decimal.html#decimal.Decimal.to_integral_value
-def round_half_up(x) -> decimal.Decimal:
-    return decimal.Decimal(x).to_integral_value(rounding=decimal.ROUND_HALF_UP)
+def round_half_up(x) -> D:
+    return D(x).to_integral_value(rounding=ROUND_HALF_UP)
 
 # https://docs.python.org/3/library/decimal.html#decimal.Decimal.quantize
-def round_half_up_digits(x, precision: int = 0) -> decimal.Decimal:
-    return decimal.Decimal(x).quantize(exp=decimal.Decimal(10)**-precision,
-                                       rounding=decimal.ROUND_HALF_UP)
+def round_half_up_digits(x, precision: int = 0) -> D:
+    return D(x).quantize(exp=D(10)**-precision, rounding=ROUND_HALF_UP)
