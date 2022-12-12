7233c28edfad5054ca41e272ff5533c548c43909
planet36 <planet36@users.noreply.github.com>
2022-12-12 12:28:44 -0500

    Update wezterm config file

--- link/.config/wezterm/wezterm.lua
+++ link/.config/wezterm/wezterm.lua
@@ -1,9 +1,16 @@
 local wezterm = require 'wezterm';
 
 return {
-  color_scheme = "Argonaut",
+  color_scheme = "Srcery (Gogh)",
   enable_scroll_bar = false,
+  enable_tab_bar = false,
   font = wezterm.font("Hack"),
   font_size = 13.0,
   scrollback_lines = 10000,
+  window_padding = {
+    left = '2px',
+    right = '0px',
+    top = '2px',
+    bottom = '0px',
+  },
 }
