275fc97d43f002756ec537788e77c34695882f4c
planet36 <planet36@users.noreply.github.com>
2022-12-14 15:48:30 -0500

    Change OUT_DIR value to be 1 directory up

--- other/generate-src/generate-files.bash
+++ other/generate-src/generate-files.bash
@@ -9,20 +9,20 @@ set -e
 
 cd "$SCRIPT_DIR"
 
-OUT_DIR="$(git rev-parse --show-toplevel)"/link/.local/include
+OUT_DIR="$(git rev-parse --show-toplevel)/link/.local"
 
-calc -d -m 4 -f generate-series-approx-coeff-aux-lat.cal > "$OUT_DIR"/aux-lat-conv.hpp
-calc -d -m 4 -f generate-series-approx-coeff-tm.cal      > "$OUT_DIR"/utm-ups-const.hpp
-python3 generate-ascii.py                                > "$OUT_DIR"/ascii.hpp
-python3 generate-gcc-machine-modes-typedefs.py           > "$OUT_DIR"/gcc-machine-modes-typedefs.hpp
-python3 generate-math-const.py                           > "$OUT_DIR"/math-const.hpp
+calc -d -m 4 -f generate-series-approx-coeff-aux-lat.cal > "$OUT_DIR"/include/aux-lat-conv.hpp
+calc -d -m 4 -f generate-series-approx-coeff-tm.cal      > "$OUT_DIR"/include/utm-ups-const.hpp
+python3 generate-ascii.py                                > "$OUT_DIR"/include/ascii.hpp
+python3 generate-gcc-machine-modes-typedefs.py           > "$OUT_DIR"/include/gcc-machine-modes-typedefs.hpp
+python3 generate-math-const.py                           > "$OUT_DIR"/include/math-const.hpp
 
 cat <<EOT
 # Run these commands:
 
 git diff \
-$OUT_DIR/{ascii,aux-lat-conv,gcc-machine-modes-typedefs,math-const,utm-ups-const}.hpp
+$OUT_DIR/include/{ascii,aux-lat-conv,gcc-machine-modes-typedefs,math-const,utm-ups-const}.hpp
 
 git commit -m 'Update generated source code files' \
-$OUT_DIR/{ascii,aux-lat-conv,gcc-machine-modes-typedefs,math-const,utm-ups-const}.hpp
+$OUT_DIR/include/{ascii,aux-lat-conv,gcc-machine-modes-typedefs,math-const,utm-ups-const}.hpp
 EOT
