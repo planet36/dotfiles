3257d7027078705b1defb498292484624ee49b85
planet36 <planet36@users.noreply.github.com>
2022-12-19 13:14:37 -0500

    Generate num_to_rgb dict when the module is imported

--- other/generate-src/generate-files.bash
+++ other/generate-src/generate-files.bash
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: Steven Ward
 # SPDX-License-Identifier: OSL-3.0
 
+# shellcheck disable=SC2034
+
 SCRIPT_NAME="$(basename -- "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
@@ -16,10 +18,7 @@ calc -d -m 4 -f generate-series-approx-coeff-tm.cal      > "$OUT_DIR"/include/ut
 python3 generate-ascii.py                                > "$OUT_DIR"/include/ascii.hpp
 python3 generate-gcc-machine-modes-typedefs.py           > "$OUT_DIR"/include/gcc-machine-modes-typedefs.hpp
 python3 generate-math-const.py                           > "$OUT_DIR"/include/math-const.hpp
-
-curl --silent --show-error https://raw.githubusercontent.com/freedesktop/xorg-xserver/master/os/oscolor.c | \
-sed -n -e '/static const BuiltinColor BuiltinColors\[\] = {/, /};/ p' | \
-python3 filter-oscolor.py > "$OUT_DIR"/lib/python/x11_colors.py
+bash generate-x11_colors.bash                            > "$OUT_DIR"/lib/python/x11_colors.py
 
 cat <<EOT
 # Run these commands:
---
+++ other/generate-src/generate-x11_colors.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+set -e
+
+curl --silent --show-error https://raw.githubusercontent.com/freedesktop/xorg-xserver/master/os/oscolor.c | \
+sed -n -e '/static const BuiltinColor BuiltinColors\[\] = {/, /};/ p' | \
+python3 filter-oscolor.py
+
+cat <<EOT
+
+rgb_vals = [0] + list(range(95, 255+1, 40))
+
+num_to_rgb = {}
+
+# Adapted from:
+# https://github.com/pygments/pygments/blob/master/pygments/formatters/terminal256.py#L172
+
+# ANSI Colors (16..231)
+for i in range(216):
+    r = rgb_vals[(i // 36) % 6]
+    g = rgb_vals[(i // 6) % 6]
+    b = rgb_vals[i % 6]
+    num = i + 16
+    num_to_rgb[num] = (r, g, b)
+
+# ANSI Grays (232..255)
+for i in range(24):
+    v = i * 10 + 8
+    num = i + 232
+    num_to_rgb[num] = (v, v, v)
+
+del rgb_vals
+EOT
