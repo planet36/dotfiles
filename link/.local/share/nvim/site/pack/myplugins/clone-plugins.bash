c854c357e026b96385580b7c46f959b5636b82fa
planet36 <planet36@users.noreply.github.com>
2022-12-21 16:48:39 -0500

    Change color schemes from opt to start

--- link/.local/share/nvim/site/pack/myplugins/clone-plugins.bash
+++ link/.local/share/nvim/site/pack/myplugins/clone-plugins.bash
@@ -10,6 +10,7 @@ SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 cd "$SCRIPT_DIR" || exit
 
 declare -a PLUGINS_START
+declare -a PLUGINS_OPT
 
 PLUGINS_START+=('https://github.com/junegunn/fzf.vim.git')
 #PLUGINS_START+=('https://github.com/tpope/vim-sensible.git')
@@ -18,16 +19,14 @@ PLUGINS_START+=('https://github.com/AndrewRadev/linediff.vim.git')
 PLUGINS_START+=('https://github.com/chrisbra/unicode.vim.git')
 PLUGINS_START+=('https://github.com/ethanholz/nvim-lastplace.git')
 
-declare -a PLUGINS_OPT
-
 # color schemes
-PLUGINS_OPT+=('https://github.com/srcery-colors/srcery-vim.git') # srcery
-PLUGINS_OPT+=('https://github.com/vim-scripts/candycode.vim.git')
-PLUGINS_OPT+=('https://github.com/fneu/breezy.git')
-PLUGINS_OPT+=('https://github.com/mhartington/oceanic-next.git') # OceanicNext
-PLUGINS_OPT+=('https://github.com/projekt0n/github-nvim-theme.git') # github_dark_default
-PLUGINS_OPT+=('https://github.com/morhetz/gruvbox.git')
-#PLUGINS_OPT+=('https://github.com/EvitanRelta/vim-colorschemes.git')
+PLUGINS_START+=('https://github.com/srcery-colors/srcery-vim.git') # srcery
+PLUGINS_START+=('https://github.com/vim-scripts/candycode.vim.git')
+PLUGINS_START+=('https://github.com/fneu/breezy.git')
+PLUGINS_START+=('https://github.com/mhartington/oceanic-next.git') # OceanicNext
+PLUGINS_START+=('https://github.com/projekt0n/github-nvim-theme.git') # github_dark_default
+PLUGINS_START+=('https://github.com/morhetz/gruvbox.git')
+#PLUGINS_START+=('https://github.com/EvitanRelta/vim-colorschemes.git')
 
 mkdir --verbose --parents -- start
 cd start || exit
