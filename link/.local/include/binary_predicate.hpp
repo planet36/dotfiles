707381539433baaa5ed64ce5333e210e27136155
Steven Ward <planet36@users.noreply.github.com>
2023-03-14 07:03:17 -0500

    Add binary_predicate.hpp, unary_predicate.hpp

---
+++ link/.local/include/binary_predicate.hpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// binary predicate concept
+/**
+\file
+\author Steven Ward
+\sa https://en.cppreference.com/w/cpp/concepts/predicate
+*/
+
+#pragma once
+
+#include <concepts>
+#include <type_traits>
+
+template <typename F, typename... Ts>
+concept binary_predicate =
+std::regular_invocable<F, Ts...> &&
+std::convertible_to<std::invoke_result_t<F, Ts...>, bool> &&
+sizeof...(Ts) == 2;
