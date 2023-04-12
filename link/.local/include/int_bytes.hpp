12f1c81e498332b000930a47f69b95c2d3375400
Steven Ward <planet36@users.noreply.github.com>
2023-04-12 12:21:48 -0500

    Add int_bits, uint_bits

--- link/.local/include/int_bytes.hpp
+++ link/.local/include/int_bytes.hpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Steven Ward
 // SPDX-License-Identifier: OSL-3.0
 
-/// compile-time typing of integers based on a given byte size
+/// compile-time typing of integers based on a given byte size or bit size
 /**
 \file
 \author Steven Ward
@@ -12,6 +12,7 @@
 
 #include <concepts>
 #include <cstdint>
+#include <limits>
 #include <type_traits>
 
 template <unsigned int N>
@@ -35,6 +36,36 @@ using uint_bytes = std::conditional_t<N <= sizeof(std::uint8_t), std::uint8_t,
                    std::conditional_t<N <= sizeof(__uint128_t), __uint128_t, void>
 #else
                    void
+#endif
+>>>>;
+
+/**
+* \sa https://en.cppreference.com/w/cpp/types/numeric_limits/digits
+*/
+template <unsigned int N>
+using int_bits = std::conditional_t<N <= std::numeric_limits<std::int8_t>::digits, std::int8_t,
+                 std::conditional_t<N <= std::numeric_limits<std::int16_t>::digits, std::int16_t,
+                 std::conditional_t<N <= std::numeric_limits<std::int32_t>::digits, std::int32_t,
+                 std::conditional_t<N <= std::numeric_limits<std::int64_t>::digits, std::int64_t,
+#if defined(__SIZEOF_INT128__)
+                 std::conditional_t<N <= std::numeric_limits<__int128_t>::digits, __int128_t, void>
+#else
+                 void
+#endif
+>>>>;
+
+/**
+* \sa https://en.cppreference.com/w/cpp/types/numeric_limits/digits
+*/
+template <unsigned int N>
+using uint_bits = std::conditional_t<N <= std::numeric_limits<std::uint8_t>::digits, std::uint8_t,
+                  std::conditional_t<N <= std::numeric_limits<std::uint16_t>::digits, std::uint16_t,
+                  std::conditional_t<N <= std::numeric_limits<std::uint32_t>::digits, std::uint32_t,
+                  std::conditional_t<N <= std::numeric_limits<std::uint64_t>::digits, std::uint64_t,
+#if defined(__SIZEOF_INT128__)
+                  std::conditional_t<N <= std::numeric_limits<__uint128_t>::digits, __uint128_t, void>
+#else
+                  void
 #endif
 >>>>;
 
