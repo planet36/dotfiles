d7681b33c0cf24d27cfda02f1517a5f61c6b1968
Steven Ward <planet36@users.noreply.github.com>
2023-04-12 12:21:18 -0500

    Add float_bits

--- link/.local/include/float_bytes.hpp
+++ link/.local/include/float_bytes.hpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Steven Ward
 // SPDX-License-Identifier: OSL-3.0
 
-/// compile-time typing of floats based on a given byte size
+/// compile-time typing of floats based on a given byte size or bit size
 /**
 \file
 \author Steven Ward
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include <limits>
 #include <type_traits>
 
 template <unsigned int N>
@@ -17,4 +18,14 @@ using float_bytes = std::conditional_t<N <= sizeof(float), float,
                     std::conditional_t<N <= sizeof(double), double,
                     std::conditional_t<N <= sizeof(long double), long double,
                     void
+>>>;
+
+/**
+* \sa https://en.cppreference.com/w/cpp/types/numeric_limits/digits
+*/
+template <unsigned int N>
+using float_bits = std::conditional_t<N <= std::numeric_limits<float>::digits, float,
+                   std::conditional_t<N <= std::numeric_limits<double>::digits, double,
+                   std::conditional_t<N <= std::numeric_limits<long double>::digits, long double,
+                   void
 >>>;
