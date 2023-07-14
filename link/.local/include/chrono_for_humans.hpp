4c92c8a8223a8255ebe6bbb3a748ec0fcef6862e
Steven Ward <planet36@users.noreply.github.com>
2023-07-14 12:09:39 -0500

    Add chrono_for_humans.hpp

---
+++ link/.local/include/chrono_for_humans.hpp
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// aliases for std::chrono built-in types
+/**
+\file
+\author Steven Ward
+\sa https://philippegroarke.com/posts/2018/chrono_for_humans/
+*/
+
+#pragma once
+
+#include <chrono>
+
+using fnanoseconds  = std::chrono::duration<float, std::chrono::nanoseconds ::period>;
+using fmicroseconds = std::chrono::duration<float, std::chrono::microseconds::period>;
+using fmilliseconds = std::chrono::duration<float, std::chrono::milliseconds::period>;
+using fseconds      = std::chrono::duration<float, std::chrono::seconds     ::period>;
+using fminutes      = std::chrono::duration<float, std::chrono::minutes     ::period>;
+using fhours        = std::chrono::duration<float, std::chrono::hours       ::period>;
+using fdays         = std::chrono::duration<float, std::chrono::days        ::period>;
+using fweeks        = std::chrono::duration<float, std::chrono::weeks       ::period>;
+using fmonths       = std::chrono::duration<float, std::chrono::months      ::period>;
+using fyears        = std::chrono::duration<float, std::chrono::years       ::period>;
+
+using dnanoseconds  = std::chrono::duration<double, std::chrono::nanoseconds ::period>;
+using dmicroseconds = std::chrono::duration<double, std::chrono::microseconds::period>;
+using dmilliseconds = std::chrono::duration<double, std::chrono::milliseconds::period>;
+using dseconds      = std::chrono::duration<double, std::chrono::seconds     ::period>;
+using dminutes      = std::chrono::duration<double, std::chrono::minutes     ::period>;
+using dhours        = std::chrono::duration<double, std::chrono::hours       ::period>;
+using ddays         = std::chrono::duration<double, std::chrono::days        ::period>;
+using dweeks        = std::chrono::duration<double, std::chrono::weeks       ::period>;
+using dmonths       = std::chrono::duration<double, std::chrono::months      ::period>;
+using dyears        = std::chrono::duration<double, std::chrono::years       ::period>;
