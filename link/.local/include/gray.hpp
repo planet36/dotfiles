6e6d997307e8862dab964adf127c5aaec9d6f329
Steven Ward <planet36@users.noreply.github.com>
2023-07-22 15:24:24 -0500

    Add gray.hpp, plural.hpp

---
+++ link/.local/include/gray.hpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Convert between binary and gray code
+/**
+\file
+\author Steven Ward
+\sa https://en.wikipedia.org/wiki/Gray_code
+*/
+
+#pragma once
+
+#include <concepts>
+#include <limits>
+
+auto binary_to_gray(std::unsigned_integral auto x) -> decltype(x)
+{
+	return x ^ (x >> 1);
+}
+
+auto gray_to_binary(std::unsigned_integral auto x) -> decltype(x)
+{
+	unsigned short shift = std::numeric_limits<decltype(x)>::digits;
+
+	while (shift /= 2)
+	{
+		x ^= (x >> shift);
+	}
+
+	return x;
+}
---
+++ link/.local/include/plural.hpp
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Determine if an integer should have a plural suffix
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <array>
+#include <concepts>
+#include <string>
+
+constexpr bool
+is_plural(const std::signed_integral auto n)
+{
+	return (n != 1) && (n != -1);
+}
+
+constexpr bool
+is_plural(const std::unsigned_integral auto n)
+{
+	return (n != 1);
+}
+
+inline constexpr std::array<std::string, 2> plural_suffix{"", "s"};
