b21ace3aeb434ce40a7c227dca49aa572084db9c
Steven Ward <planet36@users.noreply.github.com>
2023-08-08 09:41:24 -0500

    Add clmulrand.hpp

---
+++ link/.local/include/clmulrand.hpp
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Carry-less Multiplication (CLMUL) PRNG
+/**
+\file
+\author Steven Ward
+\sa https://en.wikipedia.org/wiki/CLMUL_instruction_set
+\sa https://www.felixcloutier.com/x86/pclmulqdq
+*/
+
+#pragma once
+
+#include "byteprimes.hpp"
+#include "def_urbg_class_details.hpp"
+#include "mum_mix.hpp"
+#include "simd-types.hpp"
+
+DEF_URBG_CLASS(clmulrand, simd128, uint64_t)
+{
+	static constexpr simd128 inc{.u64{byteprimes[0], byteprimes[1]}};
+	static_assert(inc.u64[0] & 1, "must be odd");
+	static_assert(inc.u64[1] & 1, "must be odd");
+
+	s.u64 += inc.u64;
+	return clmul_mix_sub(s);
+}
