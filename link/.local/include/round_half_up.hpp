dd2d9f6df80dddfeb9beff780f5b5c1a2dff0645
Steven Ward <planet36@users.noreply.github.com>
2023-08-09 15:34:16 -0500

    Add round_half_up.hpp

---
+++ link/.local/include/round_half_up.hpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// round-half-up function
+/**
+\file
+\author Steven Ward
+\sa https://www.clivemaxfield.com/diycalculator/popup-m-round.shtml
+\sa https://www.eetimes.com/an-introduction-to-different-rounding-algorithms/
+*/
+
+#pragma once
+
+#include "next.hpp"
+
+#include <cmath>
+#include <concepts>
+
+/// Round to the nearest integer, with halfway cases rounding away from zero.
+/**
+This is faster than \c std::round.
+\sa https://en.cppreference.com/w/cpp/numeric/math/round
+*/
+template <std::floating_point T>
+constexpr T round_half_up(const T x)
+{
+	// NOTE: next-less-than 0.5 is necessary when x == +/-0.49999999999999994
+	static constexpr T almost_half = next_less(T{0.5});
+
+	return std::trunc(x + std::copysign(almost_half, x));
+}
