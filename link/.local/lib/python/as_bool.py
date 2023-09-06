f362af314a483c2391e05ed3b098d4a12b06dcd8
Steven Ward <planet36@users.noreply.github.com>
2023-09-06 15:25:16 -0400

    Add as_bool.py

---
+++ link/.local/lib/python/as_bool.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+# pylint: disable=invalid-name
+
+'''
+See also
+https://docs.python.org/3/library/configparser.html#configparser.ConfigParser.getboolean
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+'''
+
+__author__ = 'Steven Ward'
+__license__ = 'OSL-3.0'
+
+def as_bool(s: str) -> bool:
+    '''Evaluate \a s as a bool.'''
+
+    if s is None:
+        return False
+
+    if type(s) is bool: # pylint: disable=unidiomatic-typecheck
+        return s
+
+    s = s.strip().lower()
+
+    if s in ("", "0", "no", "n", "false", "off",
+             "null", "nil", "none", "nan", "undefined", "-0"):
+        return False
+
+    if s in ("1", "yes", "y", "true", "on"):
+        return True
+
+    # else: vacuous truth
+    return True
