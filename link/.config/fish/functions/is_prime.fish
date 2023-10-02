d27c11da873fb246c1db869ec009fd725e80a66d
Steven Ward <planet36@users.noreply.github.com>
2023-10-02 12:39:44 -0400

    Use openssl-prime instead of factor

--- link/.config/fish/functions/is_prime.fish
+++ link/.config/fish/functions/is_prime.fish
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: OSL-3.0
 
 function is_prime --argument-names NUM --description 'Determine if a number is prime'
-    set PRIME_REGEX '^([0-9]+): \1$'
-    factor -- $NUM | string match --regex --quiet $PRIME_REGEX
+    #set PRIME_REGEX '^([0-9]+): \1$'
+    #factor -- $NUM | string match --regex --quiet $PRIME_REGEX
+    openssl prime -- $NUM | grep -F -q 'is prime'
 end
