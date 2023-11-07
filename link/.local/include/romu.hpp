65c14c677d1696d141541150f6c5ca3d37926899
Steven Ward <planet36@users.noreply.github.com>
2023-11-07 12:51:36 -0500

    Add romu_quad32, romu_trio32

--- link/.local/include/romu.hpp
+++ link/.local/include/romu.hpp
@@ -67,3 +67,36 @@ DEF_URBG_CLASS(romu_duo_jr, SINGLE_ARG(std::array<uint64_t, 2>), uint64_t)
 	s[1] = std::rotl(old_s[1] - old_s[0], R1); // (SDW)
 	return old_s[0];
 }
+
+DEF_URBG_CLASS(romu_quad32, SINGLE_ARG(std::array<uint32_t, 4>), uint32_t)
+{
+	static constexpr uint32_t M1 = 0xc61d672b; // not prime (popcount = 17)
+	static_assert(M1 & 1, "must be odd");
+	static constexpr unsigned int R1 = 26;
+	static constexpr unsigned int R2 = 9;
+
+	const auto old_s = s;
+
+	s[0] = M1 * old_s[3] + 1;                  // a-mult (SDW)
+	s[1] = old_s[3] + std::rotl(old_s[0], R1); // b-rotl, c-add
+	s[2] = old_s[2] - old_s[1];                // d-sub
+	s[3] = std::rotl(old_s[2] + old_s[0], R2); // e-add, f-rotl (SDW)
+	return old_s[1];
+}
+
+DEF_URBG_CLASS(romu_trio32, SINGLE_ARG(std::array<uint32_t, 3>), uint32_t)
+{
+	static constexpr uint32_t M1 = 0xc61d672b; // not prime (popcount = 17)
+	static_assert(M1 & 1, "must be odd");
+	static constexpr unsigned int R1 = 6;
+	static constexpr unsigned int R2 = 22;
+
+	const auto old_s = s;
+
+	s[0] = M1 * old_s[2] + 1; // (SDW)
+	s[1] = std::rotl(old_s[1] - old_s[0], R1); // (SDW)
+	s[2] = std::rotl(old_s[2] - old_s[1], R2); // (SDW)
+	return old_s[0];
+}
+
+// XXX: RomuMono32 returns uint16_t
