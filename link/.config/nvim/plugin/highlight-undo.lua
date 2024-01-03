ab702e1af925d0f0be3284e861317989d380683b
Steven Ward <planet36@users.noreply.github.com>
2024-01-03 16:18:11 -0500

    Add highlight-undo.lua

---
+++ link/.config/nvim/plugin/highlight-undo.lua
@@ -0,0 +1,21 @@
+-- Copied from:
+-- https://github.com/tzachar/highlight-undo.nvim#setup
+
+require('highlight-undo').setup({
+  duration = 300,
+  undo = {
+    hlgroup = 'HighlightUndo',
+    mode = 'n',
+    lhs = 'u',
+    map = 'undo',
+    opts = {}
+  },
+  redo = {
+    hlgroup = 'HighlightUndo',
+    mode = 'n',
+    lhs = '<C-r>',
+    map = 'redo',
+    opts = {}
+  },
+  highlight_for_count = true,
+})
