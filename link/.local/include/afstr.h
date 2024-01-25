23f42306163b0e2aae171c4f7b02a29201d2f482
Steven Ward <planet36@users.noreply.github.com>
2024-01-24 21:48:44 -0500

    Add afstr.h

---
+++ link/.local/include/afstr.h
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// automatically free string
+/**
+\file
+\author Steven Ward
+\sa https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html
+
+Example usage
+\code{.c}
+const char* s = "Hello, world!";
+// Use "afstr" as the declaration of a dynamically allocated string.
+afstr s2 = strdup(s);
+// When s2 goes out of scope, it will be automatically freed.
+\endcode
+*/
+
+#pragma once
+
+#include <stdlib.h>
+
+#define afstr \
+	__attribute__((cleanup(cleanup_free_str))) char*
+
+static void
+cleanup_free_str(char** spp)
+{
+	free(*spp);
+}
