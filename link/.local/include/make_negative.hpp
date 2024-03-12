e907662619cf84374151dc58d07080bf90cfbdcc
Steven Ward <planet36@users.noreply.github.com>
2024-03-12 16:51:49 -0400

    Add make_negative.hpp

---
+++ link/.local/include/make_negative.hpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Make a signed arithmetic value negative
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <cmath>
+#include <concepts>
+
+constexpr auto
+make_negative(const std::signed_integral auto x)
+{
+	if (x < 0)
+		return x;
+	else
+		return -x;
+}
+
+constexpr auto
+make_negative(const std::floating_point auto x)
+{
+	if (std::signbit(x))
+		return x;
+	else
+		return std::copysign(x, decltype(x){-1});
+}
