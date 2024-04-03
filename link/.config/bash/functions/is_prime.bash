2542fc8ac51aff1986dd4d4c368e6975f1e72651
Steven Ward <planet36@users.noreply.github.com>
2024-04-03 16:58:28 -0400

    Remove is_prime shell functions

--- link/.config/bash/functions/is_prime.bash
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Steven Ward
-# SPDX-License-Identifier: OSL-3.0
-
-function is_prime
-{
-    factor -- "${1}" | grep --perl-regexp --count '^([[:digit:]]+): \1$'
-}
--- link/.config/fish/functions/is_prime.fish
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Steven Ward
-# SPDX-License-Identifier: OSL-3.0
-
-function is_prime --argument-names NUM --description 'Determine if a number is prime'
-    #set PRIME_REGEX '^([0-9]+): \1$'
-    #factor -- $NUM | string match --regex --quiet $PRIME_REGEX
-    openssl prime -- $NUM | grep -F -q 'is prime'
-end
