9b0e26aae46b48f801ed8cd475a7d4b892a56b64
Steven Ward <planet36@users.noreply.github.com>
2024-04-04 19:56:47 -0400

    Add NODE_REPL_HISTORY env var

--- link/.config/bash/envvars.bash
+++ link/.config/bash/envvars.bash
@@ -77,6 +77,8 @@ export LESSHISTFILE=/dev/null
 # This affects meld.
 export NO_AT_BRIDGE=1
 
+export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
+
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
 
 # https://www.reddit.com/r/linux/comments/12wxsl/whats_in_your_bashrc/c6z0y5g/
