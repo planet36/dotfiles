266b6299650fb8bd1664ffd18bb85c637ef10323
Steven Ward <planet36@users.noreply.github.com>
2024-04-12 13:31:03 -0400

    Add map_int.hpp

---
+++ link/.local/include/map_int.hpp
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Map an unsigned integer to an interval
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include "int_bytes.hpp"
+
+#include <concepts>
+#include <limits>
+
+/// map \a x to the inverval <code>[a, b]</code>
+/**
+\pre \a a <= \a b
+\retval \a x if <code>a == MIN</code> and <code>b == MAX</code>
+*/
+template <std::unsigned_integral T>
+constexpr T
+map_int(const T x, const T a, const T b)
+{
+	if ((a == std::numeric_limits<T>::min()) && (b == std::numeric_limits<T>::max()))
+		// no mapping needed
+		return x;
+
+	const T interval_range{b - a + 1};
+	const auto product = widen(x) * widen(interval_range);
+	return static_cast<T>(product >> std::numeric_limits<T>::digits) + a;
+}
