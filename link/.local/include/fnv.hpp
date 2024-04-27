9b1dfffe62d2a31ef9c8b8a3ef03201fa226010c
Steven Ward <planet36@users.noreply.github.com>
2024-04-27 16:01:59 -0400

    Store FNV constants in namespaces

--- link/.local/include/fnv.hpp
+++ link/.local/include/fnv.hpp
@@ -16,26 +16,32 @@
 #include <iterator>
 #include <string_view>
 
-/*
-https://www.wolframalpha.com/input?i=PrimeQ%5B16777619%5D
-https://www.wolframalpha.com/input?i=PrimeQ%5B1099511628211%5D
-*/
-inline constexpr uint32_t fnv_prime_32 = UINT32_C(16'777'619); // = 2**24 + 2**8 + 0x93
-inline constexpr uint64_t fnv_prime_64 = UINT64_C(1'099'511'628'211); // = 2**40 + 2**8 + 0xB3
+namespace fnv_const_32
+{
+// https://www.wolframalpha.com/input?i=PrimeQ%5B16777619%5D
+inline constexpr uint32_t fnv_prime = UINT32_C(16'777'619); // = 2**24 + 2**8 + 0x93
+inline constexpr uint32_t fnv_offset_basis = UINT32_C(2'166'136'261);
+}
 
-inline constexpr uint32_t fnv_offset_basis_32 = UINT32_C(2'166'136'261);
-inline constexpr uint64_t fnv_offset_basis_64 = UINT64_C(14'695'981'039'346'656'037);
+namespace fnv_const_64
+{
+// https://www.wolframalpha.com/input?i=PrimeQ%5B1099511628211%5D
+inline constexpr uint64_t fnv_prime = UINT64_C(1'099'511'628'211); // = 2**40 + 2**8 + 0xB3
+inline constexpr uint64_t fnv_offset_basis = UINT64_C(14'695'981'039'346'656'037);
+}
 
 /// FNV-1 32-bit hash
 constexpr auto
 fnv1_32(const std::string_view s)
 {
-	auto result = fnv_offset_basis_32;
+	using namespace fnv_const_32;
+
+	auto result = fnv_offset_basis;
 
 	for (const auto c : s)
 	{
 		const auto octet = static_cast<uint8_t>(c);
-		result *= fnv_prime_32;
+		result *= fnv_prime;
 		result ^= octet;
 	}
 
@@ -46,13 +52,15 @@ fnv1_32(const std::string_view s)
 constexpr auto
 fnv1a_32(const std::string_view s)
 {
-	auto result = fnv_offset_basis_32;
+	using namespace fnv_const_32;
+
+	auto result = fnv_offset_basis;
 
 	for (const auto c : s)
 	{
 		const auto octet = static_cast<uint8_t>(c);
 		result ^= octet;
-		result *= fnv_prime_32;
+		result *= fnv_prime;
 	}
 
 	return result;
@@ -62,12 +70,14 @@ fnv1a_32(const std::string_view s)
 constexpr auto
 fnv1_64(const std::string_view s)
 {
-	auto result = fnv_offset_basis_64;
+	using namespace fnv_const_64;
+
+	auto result = fnv_offset_basis;
 
 	for (const auto c : s)
 	{
 		const auto octet = static_cast<uint8_t>(c);
-		result *= fnv_prime_64;
+		result *= fnv_prime;
 		result ^= octet;
 	}
 
@@ -78,13 +88,15 @@ fnv1_64(const std::string_view s)
 constexpr auto
 fnv1a_64(const std::string_view s)
 {
-	auto result = fnv_offset_basis_64;
+	using namespace fnv_const_64;
+
+	auto result = fnv_offset_basis;
 
 	for (const auto c : s)
 	{
 		const auto octet = static_cast<uint8_t>(c);
 		result ^= octet;
-		result *= fnv_prime_64;
+		result *= fnv_prime;
 	}
 
 	return result;
@@ -96,11 +108,13 @@ requires (sizeof(std::iter_value_t<Iter>) == 1)
 constexpr auto
 fnv1_32(Iter first, const Iter last)
 {
-	auto result = fnv_offset_basis_32;
+	using namespace fnv_const_32;
+
+	auto result = fnv_offset_basis;
 
 	while (first != last)
 	{
-		result *= fnv_prime_32;
+		result *= fnv_prime;
 		result ^= static_cast<uint8_t>(*first);
 		++first;
 	}
@@ -114,12 +128,14 @@ requires (sizeof(std::iter_value_t<Iter>) == 1)
 constexpr auto
 fnv1a_32(Iter first, const Iter last)
 {
-	auto result = fnv_offset_basis_32;
+	using namespace fnv_const_32;
+
+	auto result = fnv_offset_basis;
 
 	while (first != last)
 	{
 		result ^= static_cast<uint8_t>(*first);
-		result *= fnv_prime_32;
+		result *= fnv_prime;
 		++first;
 	}
 
@@ -132,11 +148,13 @@ requires (sizeof(std::iter_value_t<Iter>) == 1)
 constexpr auto
 fnv1_64(Iter first, const Iter last)
 {
-	auto result = fnv_offset_basis_64;
+	using namespace fnv_const_64;
+
+	auto result = fnv_offset_basis;
 
 	while (first != last)
 	{
-		result *= fnv_prime_64;
+		result *= fnv_prime;
 		result ^= static_cast<uint8_t>(*first);
 		++first;
 	}
@@ -150,12 +168,14 @@ requires (sizeof(std::iter_value_t<Iter>) == 1)
 constexpr auto
 fnv1a_64(Iter first, const Iter last)
 {
-	auto result = fnv_offset_basis_64;
+	using namespace fnv_const_64;
+
+	auto result = fnv_offset_basis;
 
 	while (first != last)
 	{
 		result ^= static_cast<uint8_t>(*first);
-		result *= fnv_prime_64;
+		result *= fnv_prime;
 		++first;
 	}
 
