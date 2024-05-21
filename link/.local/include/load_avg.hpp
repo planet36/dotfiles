344816cefef3b340903fc1820eca89346aa5a786
Steven Ward <planet36@users.noreply.github.com>
2024-05-21 12:08:38 -0400

    Fix error: conversion from 'double' to 'std::vector<long int>::value_type' {aka 'long int'} may change value [-Werror=float-conversion]

--- link/.local/include/load_avg.hpp
+++ link/.local/include/load_avg.hpp
@@ -57,7 +57,7 @@ get_load_avg_int(int num_samples = max_samples)
 
 	for (const auto& x : load_avg)
 	{
-		load_avg_int.push_back(x * factor);
+		load_avg_int.push_back(static_cast<decltype(load_avg_int)::value_type>(x * factor));
 	}
 
 	return load_avg_int;
