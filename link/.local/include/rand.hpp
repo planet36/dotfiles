4df95e0d54607277f4dc3d3937e97628e4f41903
Steven Ward <planet36@users.noreply.github.com>
2024-05-21 12:53:46 -0400

    Make change similar to earlier commit

--- link/.local/include/rand.hpp
+++ link/.local/include/rand.hpp
@@ -161,7 +161,7 @@ rand_double(G& gen)
 	static_assert(std::numeric_limits<typename G::result_type>::digits >=
 	                  std::numeric_limits<T>::digits,
 	              "PRNG does not provide sufficient bits");
-	return make_unit_double(gen());
+	return make_unit_double(static_cast<uint_bytes<sizeof(T)>>(gen()));
 #else
 	if constexpr (std::numeric_limits<T>::digits <=
 	              std::numeric_limits<typename G::result_type>::digits)
@@ -202,7 +202,7 @@ rand_long_double(G& gen)
 	static_assert(std::numeric_limits<typename G::result_type>::digits >=
 	                  std::numeric_limits<T>::digits,
 	              "PRNG does not provide sufficient bits");
-	return make_unit_long_double(gen());
+	return make_unit_long_double(static_cast<uint_bytes<sizeof(T)>>(gen()));
 #else
 	if constexpr (std::numeric_limits<T>::digits <=
 	              std::numeric_limits<typename G::result_type>::digits)
