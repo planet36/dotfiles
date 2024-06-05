bfdf96ede4b0e5de9c5471c82e2dc6fce967f435
Steven Ward <planet36@users.noreply.github.com>
2024-06-05 14:05:56 -0400

    Use bit_patterns_64::C instead of _mum_primes

--- link/.local/include/mix.hpp
+++ link/.local/include/mix.hpp
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include "bit_patterns.hpp"
 #include "mum.hpp"
 
 #include <concepts>
@@ -18,64 +19,70 @@
 constexpr void
 mumx_mix_u64x2(uint64_t& x0, uint64_t& x1)
 {
+	using namespace bit_patterns_64;
+
 	// step 0
-	x0 += _mum_primes[0];
-	x1 += _mum_primes[1];
+	x0 += C[shuf_i[0]];
+	x1 += C[shuf_i[1]];
 
 	// step 1 -- keep x0
-	x1 = mumx(x1, x0 + _mum_primes[2]);
+	x1 = mumx(x1, x0 + C[shuf_i[2]]);
 
 	// step 2 -- keep x1
-	x0 = mumx(x0, x1 + _mum_primes[3]);
+	x0 = mumx(x0, x1 + C[shuf_i[3]]);
 }
 
 constexpr void
 mumx_mix_u64x3(uint64_t& x0, uint64_t& x1, uint64_t& x2)
 {
+	using namespace bit_patterns_64;
+
 	// step 0
-	x0 += _mum_primes[0];
-	x1 += _mum_primes[1];
-	x2 += _mum_primes[2];
+	x0 += C[shuf_i[0]];
+	x1 += C[shuf_i[1]];
+	x2 += C[shuf_i[2]];
 
 	// step 1 -- keep x0
-	x1 = mumx(x1, x0 + _mum_primes[3]);
-	x2 = mumx(x2, x0 + _mum_primes[4]);
+	x1 = mumx(x1, x0 + C[shuf_i[3]]);
+	x2 = mumx(x2, x0 + C[shuf_i[4]]);
 
 	// step 2 -- keep x1
-	x0 = mumx(x0, x1 + _mum_primes[5]);
-	x2 = mumx(x2, x1 + _mum_primes[6]);
+	x0 = mumx(x0, x1 + C[shuf_i[5]]);
+	x2 = mumx(x2, x1 + C[shuf_i[6]]);
 
 	// step 3 -- keep x2
-	x0 = mumx(x0, x2 + _mum_primes[7]);
-	x1 = mumx(x1, x2 + _mum_primes[8]);
+	x0 = mumx(x0, x2 + C[shuf_i[7]]);
+	x1 = mumx(x1, x2 + C[shuf_i[8]]);
 }
 
 constexpr void
 mumx_mix_u64x4(uint64_t& x0, uint64_t& x1, uint64_t& x2, uint64_t& x3)
 {
+	using namespace bit_patterns_64;
+
 	// step 0
-	x0 += _mum_primes[0];
-	x1 += _mum_primes[1];
-	x2 += _mum_primes[2];
-	x3 += _mum_primes[3];
+	x0 += C[shuf_i[0]];
+	x1 += C[shuf_i[1]];
+	x2 += C[shuf_i[2]];
+	x3 += C[shuf_i[3]];
 
 	// step 1 -- keep x0
-	x1 = mumx(x1, x0 + _mum_primes[4]);
-	x2 = mumx(x2, x0 + _mum_primes[5]);
-	x3 = mumx(x3, x0 + _mum_primes[6]);
+	x1 = mumx(x1, x0 + C[shuf_i[4]]);
+	x2 = mumx(x2, x0 + C[shuf_i[5]]);
+	x3 = mumx(x3, x0 + C[shuf_i[6]]);
 
 	// step 2 -- keep x1
-	x0 = mumx(x0, x1 + _mum_primes[7]);
-	x2 = mumx(x2, x1 + _mum_primes[8]);
-	x3 = mumx(x3, x1 + _mum_primes[9]);
+	x0 = mumx(x0, x1 + C[shuf_i[7]]);
+	x2 = mumx(x2, x1 + C[shuf_i[8]]);
+	x3 = mumx(x3, x1 + C[shuf_i[9]]);
 
 	// step 3 -- keep x2
-	x0 = mumx(x0, x2 + _mum_primes[10]);
-	x1 = mumx(x1, x2 + _mum_primes[11]);
-	x3 = mumx(x3, x2 + _mum_primes[12]);
+	x0 = mumx(x0, x2 + C[shuf_i[10]]);
+	x1 = mumx(x1, x2 + C[shuf_i[11]]);
+	x3 = mumx(x3, x2 + C[shuf_i[12]]);
 
 	// step 4 -- keep x3
-	x0 = mumx(x0, x3 + _mum_primes[13]);
-	x1 = mumx(x1, x3 + _mum_primes[14]);
-	x2 = mumx(x2, x3 + _mum_primes[15]);
+	x0 = mumx(x0, x3 + C[shuf_i[13]]);
+	x1 = mumx(x1, x3 + C[shuf_i[14]]);
+	x2 = mumx(x2, x3 + C[shuf_i[15]]);
 }
