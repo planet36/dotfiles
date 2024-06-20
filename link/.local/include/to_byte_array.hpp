a0ed257104290289d2a5514eb416f07b4499bb6b
Steven Ward <planet36@users.noreply.github.com>
2024-06-20 08:11:40 -0400

    Add to_byte_array.hpp

---
+++ link/.local/include/to_byte_array.hpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Convert an object to a \c std::array of \c std::byte
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <array>
+#include <bit>
+#include <cstddef>
+
+constexpr auto
+to_byte_array(const auto& x)
+{
+	return std::bit_cast<std::array<std::byte, sizeof(x)>>(x);
+}
