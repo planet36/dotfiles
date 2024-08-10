28d503eee5885f9c7fae301720886483d2bb6e98
Steven Ward <planet36@users.noreply.github.com>
2024-08-09 22:59:44 -0400

    Add gray.py

---
+++ link/.local/lib/python/gray.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+# pylint: disable=invalid-name
+# pylint: disable=missing-module-docstring
+
+__author__ = 'Steven Ward'
+__license__ = 'OSL-3.0'
+
+# https://rosettacode.org/wiki/Gray_code#Python
+
+def bin_to_gray(n : int) -> int:
+    '''Convert binary integer to gray code.'''
+    return n ^ (n >> 1)
+
+
+def gray_to_bin(n : int) -> int:
+    '''Convert gray code to binary integer.'''
+    mask = n
+    while (mask := mask >> 1):
+        n ^= mask
+    return n
