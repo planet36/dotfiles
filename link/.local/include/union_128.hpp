5fc72cef45901101557daeba9465bc505123b86a
Steven Ward <planet36@users.noreply.github.com>
2024-09-11 12:40:38 -0400

    Add union_128.hpp

---
+++ link/.local/include/union_128.hpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Union of size 128 bits with integer data members
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <immintrin.h>
+
+union alignas(__m128i) union_128
+{
+	std::array<std::byte, 16/sizeof(std::byte)> bytes;
+	std::array<uint8_t, 16/sizeof(uint8_t)> u8;
+	std::array<uint16_t, 16/sizeof(uint16_t)> u16;
+	std::array<uint32_t, 16/sizeof(uint32_t)> u32;
+	std::array<uint64_t, 16/sizeof(uint64_t)> u64;
+#if defined(__SIZEOF_INT128__)
+	__int128_t i128;
+	__uint128_t u128;
+#endif
+	__m128i xmm;
+};
+
+static_assert(sizeof(union_128) == 128/8);
