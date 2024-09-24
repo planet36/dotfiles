34fe72069c940f94cce9188f4799b6e6609995db
Steven Ward <planet36@users.noreply.github.com>
2024-09-24 07:35:08 -0400

    Add vne.h

---
+++ link/.local/include/vne.h
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Von Neumann extractor
+/**
+\file
+\author Steven Ward
+\sa https://en.wikipedia.org/wiki/Randomness_extractor#Von_Neumann_extractor
+*/
+
+#pragma once
+
+inline int
+von_neumann_extractor(unsigned char x)
+{
+	switch (x & 0b11)
+	{
+	case 0b10: return 0;
+	case 0b01: return 1;
+	default: return -1;
+	}
+}
