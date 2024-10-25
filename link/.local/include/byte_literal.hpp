ad1a1705e9b46aec0b04e326646cc86c024e7b99
Steven Ward <planet36@users.noreply.github.com>
2024-10-24 21:18:33 -0400

    Add byte_literal.hpp

---
+++ link/.local/include/byte_literal.hpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// User-defined integer literal to create a \c std::byte
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <cstddef>
+
+constexpr std::byte
+operator""_b(unsigned long long x)
+{
+	return std::byte{static_cast<unsigned char>(x)};
+}
