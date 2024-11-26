01fad10f701cfe12d23ec45182c8334d548a504e
Steven Ward <planet36@users.noreply.github.com>
2024-11-25 22:13:07 -0500

    Sync with original source

--- link/.config/nvim/plugin/highlight-undo.lua
+++ link/.config/nvim/plugin/highlight-undo.lua
@@ -1,20 +1,4 @@
 -- Copied from:
 -- https://github.com/tzachar/highlight-undo.nvim#setup
 
-require('highlight-undo').setup({
-  duration = 300,
-  keymaps = {
-    Keymap_name = {
-      -- most fields here are the same as in vim.keymap.set
-      desc = "a description",
-      hlgroup = 'HighlightUndo',
-      mode = 'n',
-      lhs = 'lhs',
-      rhs = 'optional, can be nil',
-      opts = {
-        -- same as opts to vim.keymap.set. if rhs is nil, there should be a
-        -- callback key which points to a function
-      },
-    },
-  },
-})
+require('highlight-undo').setup({})
