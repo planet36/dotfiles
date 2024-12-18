2d4c2ee421242a4d9d289eb985e6b487101850d5
Steven Ward <planet36@users.noreply.github.com>
2024-12-18 13:11:35 -0500

    Fix syntax error

--- link/.local/include/clmum.h
+++ link/.local/include/clmum.h
@@ -24,8 +24,8 @@ static void
 clmul_u64(uint64_t* hi, uint64_t* lo) [[gnu::nonnull]]
 {
 	// order of hi, lo doesn't matter
-	__m128i result = _mm_set_epi64x(static_cast<long long int>(*hi),
-	                                static_cast<long long int>(*lo));
+	__m128i result = _mm_set_epi64x((long long int)(*hi),
+	                                (long long int)(*lo));
 
 	// https://clang.llvm.org/doxygen/____wmmintrin__pclmul_8h.html
 	// https://github.com/gcc-mirror/gcc/blob/master/gcc/config/i386/wmmintrin.h#L103
@@ -38,8 +38,8 @@ clmul_u64(uint64_t* hi, uint64_t* lo) [[gnu::nonnull]]
 	// MSB in result is always 0
 	result = _mm_clmulepi64_si128(result, result, 0x10);
 
-	*hi = static_cast<uint64_t>(_mm_extract_epi64(result, 1));
-	*lo = static_cast<uint64_t>(_mm_extract_epi64(result, 0));
+	*hi = (uint64_t)(_mm_extract_epi64(result, 1));
+	*lo = (uint64_t)(_mm_extract_epi64(result, 0));
 }
 
 static inline uint64_t
