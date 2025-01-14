64aaf7b0b8f5d066bf19726cd0cdd4a3970e5b2d
Steven Ward <planet36@users.noreply.github.com>
2025-01-14 11:11:03 -0500

    Add byte_width.hpp

---
+++ link/.local/include/byte_width.hpp
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Get the byte width of an unsigned integer
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <algorithm>
+#include <bit>
+
+constexpr unsigned int
+byte_width(const size_t x)
+{
+	// std::bit_width(0) returns 0, but we want it to be 1
+	const unsigned int w = std::max(std::bit_width(x), 1);
+	return (w / 8) + (w % 8 != 0);
+}
