f7a8312a601af65e5757edb862f1ee7c8338054c
Steven Ward <planet36@users.noreply.github.com>
2025-01-20 22:21:21 -0500

    Add to_vec_bytes.hpp

---
+++ link/.local/include/to_vec_bytes.hpp
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Convert a container to a \c std::vector of \c std::byte
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <ranges>
+#include <span>
+#include <vector>
+
+auto
+to_vec_bytes(const std::ranges::contiguous_range auto & container)
+{
+	const auto sp_bytes = std::as_bytes(std::span(container));
+	return std::vector(std::cbegin(sp_bytes), std::cend(sp_bytes));
+}
