9aecfc527ac1d1a7a34f20f0c9bf251f805a18f2
Steven Ward <planet36@users.noreply.github.com>
2025-02-26 16:01:34 -0500

    Add union simd_256

--- link/.local/include/simd-union.hpp
+++ link/.local/include/simd-union.hpp
@@ -31,3 +31,22 @@ union alignas(__m128i) simd_128
 };
 
 static_assert(sizeof(simd_128) == 16);
+
+union alignas(__m256i) simd_256
+{
+	std::array<std::byte, 32 / sizeof(std::byte)> bytes;
+	std::array<uint8_t, 32 / sizeof(uint8_t)> u8;
+	std::array<uint16_t, 32 / sizeof(uint16_t)> u16;
+	std::array<uint32_t, 32 / sizeof(uint32_t)> u32;
+	std::array<uint64_t, 32 / sizeof(uint64_t)> u64;
+#if defined(__SIZEOF_INT128__)
+	std::array<__uint128_t, 32 / sizeof(__uint128_t)> u128;
+#endif
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-attributes"
+	std::array<__m128i, 32 / sizeof(__m128i)> xmm;
+	std::array<__m256i, 32 / sizeof(__m256i)> ymm;
+#pragma GCC diagnostic pop
+};
+
+static_assert(sizeof(simd_256) == 32);
