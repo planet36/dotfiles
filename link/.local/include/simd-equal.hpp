71548af5c45491d1cd0e2a3106f357db0951488f
Steven Ward <planet36@users.noreply.github.com>
2025-02-26 16:12:11 -0500

    Add simd-equal.hpp

---
+++ link/.local/include/simd-equal.hpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Test if two SIMD values are equal
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include "simd-concepts.hpp"
+
+#include <cstring>
+
+template <simd_t T>
+inline bool
+simd_equal(const T& a, const T& b) { return std::memcmp(&a, &b, sizeof(T)) == 0; }
