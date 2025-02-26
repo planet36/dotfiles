9f0d9e3314756cc0a80fc9ad94c57904c3d1a71a
Steven Ward <planet36@users.noreply.github.com>
2025-02-26 16:11:11 -0500

    Add simd-concepts.hpp

---
+++ link/.local/include/simd-concepts.hpp
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Concepts for SIMD types
+/**
+\file
+\author Steven Ward
+Typedefs defined in:
+- xmmintrin.h
+- emmintrin.h
+- avxintrin.h
+- avx512fintrin.h
+*/
+
+#pragma once
+
+#include <concepts>
+#include <immintrin.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-attributes"
+
+template <typename T>
+concept simd_float_t = std::same_as<T, __m128> ||
+                       std::same_as<T, __m256> ||
+                       std::same_as<T, __m512>;
+
+template <typename T>
+concept simd_double_t = std::same_as<T, __m128d> ||
+                        std::same_as<T, __m256d> ||
+                        std::same_as<T, __m512d>;
+
+template <typename T>
+concept simd_int_t = std::same_as<T, __m128i> ||
+                     std::same_as<T, __m256i> ||
+                     std::same_as<T, __m512i>;
+
+#pragma GCC diagnostic pop
+
+template <typename T>
+concept simd_t = simd_float_t<T> || simd_double_t<T> || simd_int_t<T>;
