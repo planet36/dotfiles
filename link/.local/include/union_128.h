6be74736892c8f63a2674dfc7b4250deb5fb9cbe
Steven Ward <planet36@users.noreply.github.com>
2025-02-26 14:36:49 -0500

    Use consistent syntax

--- link/.local/include/union_128.h
+++ link/.local/include/union_128.h
@@ -28,7 +28,7 @@ union union_128
 	__m128i xmm;
 };
 
-static_assert(sizeof(union union_128) == 128/8);
+static_assert(sizeof(union union_128) == 16);
 
 #ifdef __cplusplus
 } // extern "C"
--- link/.local/include/union_128.hpp
+++ link/.local/include/union_128.hpp
@@ -16,15 +16,15 @@
 
 union alignas(__m128i) union_128
 {
-	std::array<std::byte, 16/sizeof(std::byte)> bytes;
-	std::array<uint8_t, 16/sizeof(uint8_t)> u8;
-	std::array<uint16_t, 16/sizeof(uint16_t)> u16;
-	std::array<uint32_t, 16/sizeof(uint32_t)> u32;
-	std::array<uint64_t, 16/sizeof(uint64_t)> u64;
+	std::array<std::byte, 16 / sizeof(std::byte)> bytes;
+	std::array<uint8_t, 16 / sizeof(uint8_t)> u8;
+	std::array<uint16_t, 16 / sizeof(uint16_t)> u16;
+	std::array<uint32_t, 16 / sizeof(uint32_t)> u32;
+	std::array<uint64_t, 16 / sizeof(uint64_t)> u64;
 #if defined(__SIZEOF_INT128__)
 	__uint128_t u128;
 #endif
 	__m128i xmm;
 };
 
-static_assert(sizeof(union_128) == 128/8);
+static_assert(sizeof(union_128) == 16);
