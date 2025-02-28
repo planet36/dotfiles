c3449283f7bea0eb34b1a171d0803fe5c5151525
Steven Ward <planet36@users.noreply.github.com>
2025-02-28 10:48:58 -0500

    Add lock_fd.h

---
+++ link/.local/include/lock_fd.h
@@ -0,0 +1,79 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Acquire/release a read/write lock on a file descriptor
+/**
+\file
+\author Steven Ward
+\sa https://www.gnu.org/software/libc/manual/html_node/File-Locks.html
+\sa https://man7.org/linux/man-pages/man2/fcntl.2.html
+*/
+
+#pragma once
+
+#include <fcntl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline int
+acq_read_lock_fd(int fd)
+{
+#ifdef __cplusplus
+	using flock_t = flock;
+#else
+	typedef struct flock flock_t;
+#endif
+
+	flock_t lock = {
+		.l_type = F_RDLCK,
+		.l_whence = SEEK_SET,
+		.l_start = 0,
+		.l_len = 0,
+		.l_pid = 0,
+	};
+	return fcntl(fd, F_OFD_SETLKW, &lock); // or F_OFD_SETLK
+}
+
+static inline int
+acq_write_lock_fd(int fd)
+{
+#ifdef __cplusplus
+	using flock_t = flock;
+#else
+	typedef struct flock flock_t;
+#endif
+
+	flock_t lock = {
+		.l_type = F_WRLCK,
+		.l_whence = SEEK_SET,
+		.l_start = 0,
+		.l_len = 0,
+		.l_pid = 0,
+	};
+	return fcntl(fd, F_OFD_SETLKW, &lock); // or F_OFD_SETLK
+}
+
+static inline int
+rel_lock_fd(int fd)
+{
+#ifdef __cplusplus
+	using flock_t = flock;
+#else
+	typedef struct flock flock_t;
+#endif
+
+	flock_t lock = {
+		.l_type = F_UNLCK,
+		.l_whence = SEEK_SET,
+		.l_start = 0,
+		.l_len = 0,
+		.l_pid = 0,
+	};
+	return fcntl(fd, F_OFD_SETLKW, &lock); // or F_OFD_SETLK
+}
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
