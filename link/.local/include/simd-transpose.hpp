90d4674f276fb4add67d766a2a19a69dc70dfab7
Steven Ward <planet36@users.noreply.github.com>
2025-03-04 10:23:42 -0500

    Add brief descriptions

--- link/.local/include/simd-transpose.hpp
+++ link/.local/include/simd-transpose.hpp
@@ -14,6 +14,7 @@
 #include <array>
 #include <immintrin.h>
 
+/// Treat \a x as a 2x2 array of \c uint64_t
 inline void
 transpose_2(arr_m128i<2>& x)
 {
@@ -25,6 +26,7 @@ transpose_2(arr_m128i<2>& x)
 }
 
 
+/// Treat \a x as a 4x4 array of \c uint32_t
 /**
 \sa https://randombit.net/bitbashing/posts/integer_matrix_transpose_in_sse2.html
 */
@@ -43,6 +45,7 @@ transpose_4(arr_m128i<4>& x)
 }
 
 
+/// Treat \a x as a 4x4 array of \c uint32_t
 /**
 \sa https://community.intel.com/t5/Intel-ISA-Extensions/4x4-matrix-transpose-using-sse2-intrinsics/m-p/785883#M353
 \verbatim
@@ -66,6 +69,7 @@ transpose_4_macro(arr_m128i<4>& x)
 }
 
 
+/// Treat \a x as an 8x8 array of \c uint16_t
 /**
 \sa https://stackoverflow.com/a/4951060/1892784
 */
@@ -101,6 +105,7 @@ transpose_8(arr_m128i<8>& x)
 }
 
 
+/// Treat \a x as a 16x16 array of \c uint8_t
 void
 transpose_16(arr_m128i<16>& x)
 {
