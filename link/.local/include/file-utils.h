1cbc87da691cf20d12dad22ed2d4e4b586a7a068
Steven Ward <planet36@users.noreply.github.com>
2025-03-25 17:47:05 -0400

    Add file-utils.h

---
+++ link/.local/include/file-utils.h
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Small functions for file descriptors, etc.
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <sys/stat.h>
+#include <unistd.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline bool
+is_seekable(const int fd)
+{
+	return lseek(fd, 0, SEEK_CUR) != -1;
+}
+
+static inline long
+get_file_size(const int fd)
+{
+	struct stat statbuf;
+
+	if (fstat(fd, &statbuf) < 0)
+		return -1;
+
+	return statbuf.st_size;
+}
+
+static inline long
+get_mmap_size(const long file_size)
+{
+	const long page_size = sysconf(_SC_PAGESIZE);
+
+	if (file_size == 0)
+		return page_size;
+
+	if ((file_size % page_size) == 0)
+		return file_size;
+
+	// align on page boundary
+	return file_size + (page_size - (file_size % page_size));
+}
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
