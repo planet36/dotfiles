7136ba217d2ad984d7e60b1ee158d2b1b277b73b
Steven Ward <planet36@users.noreply.github.com>
2025-03-31 14:55:39 -0400

    Add functions make_odd_epu{8,16,32,64}

--- link/.local/include/aes.hpp
+++ link/.local/include/aes.hpp
@@ -383,3 +383,31 @@ aesdec_davies_meyer(const T H, const T m)
 	}
 	return H ^ a;
 }
+
+/// Make the packed unsigned 8-bit integers odd
+inline auto
+make_odd_epu8(const __m128i a)
+{
+	return _mm_or_si128(a, _mm_set1_epi8(1));
+}
+
+/// Make the packed unsigned 16-bit integers odd
+inline auto
+make_odd_epu16(const __m128i a)
+{
+	return _mm_or_si128(a, _mm_set1_epi16(1));
+}
+
+/// Make the packed unsigned 32-bit integers odd
+inline auto
+make_odd_epu32(const __m128i a)
+{
+	return _mm_or_si128(a, _mm_set1_epi32(1));
+}
+
+/// Make the packed unsigned 64-bit integers odd
+inline auto
+make_odd_epu64(const __m128i a)
+{
+	return _mm_or_si128(a, _mm_set1_epi64x(1));
+}
