d5225b4289c105b5b59a9bb4177a1d76cf4c3f2d
Steven Ward <planet36@users.noreply.github.com>
2025-04-02 13:08:39 -0400

    Put each lualine option on its own line

--- link/.config/nvim/init.lua
+++ link/.config/nvim/init.lua
@@ -305,15 +305,24 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {
-      { 'mode', fmt = function(str) return str:sub(1,1) end },
+      {
+        'mode',
+        fmt = function(str) return str:sub(1,1) end,
+      },
     },
     lualine_c = {
-      { 'filename', path = 1},
+      {
+        'filename',
+        path = 1,
+      },
     },
   },
   inactive_sections = {
     lualine_c = {
-      { 'filename', path = 1},
+      {
+        'filename',
+        path = 1,
+      },
     },
   },
 }
