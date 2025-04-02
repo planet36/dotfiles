bb9a5bb885ed2368cce7d3f422689f96d7d592a4
Steven Ward <planet36@users.noreply.github.com>
2025-04-02 12:59:18 -0400

    Show relative path

--- link/.config/nvim/init.lua
+++ link/.config/nvim/init.lua
@@ -307,6 +307,14 @@ require('lualine').setup {
     lualine_a = {
       { 'mode', fmt = function(str) return str:sub(1,1) end },
     },
+    lualine_c = {
+      { 'filename', path = 1},
+    },
+  },
+  inactive_sections = {
+    lualine_c = {
+      { 'filename', path = 1},
+    },
   },
 }
 
