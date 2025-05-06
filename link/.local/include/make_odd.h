94ea342a581c970a85eda46b1e14308a66308ddf
Steven Ward <planet36@users.noreply.github.com>
2025-05-06 15:02:39 -0400

    Sync with prng-test-bench repo

--- link/.local/include/make_odd.h
+++ link/.local/include/make_odd.h
@@ -11,30 +11,38 @@
 
 #include <immintrin.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /// Make the packed unsigned 8-bit integers odd
-inline __m128i
-make_odd_epu8(const __m128i a)
+static inline __m128i
+mm_make_odd_epu8(const __m128i a)
 {
     return _mm_or_si128(a, _mm_set1_epi8(1));
 }
 
 /// Make the packed unsigned 16-bit integers odd
-inline __m128i
-make_odd_epu16(const __m128i a)
+static inline __m128i
+mm_make_odd_epu16(const __m128i a)
 {
     return _mm_or_si128(a, _mm_set1_epi16(1));
 }
 
 /// Make the packed unsigned 32-bit integers odd
-inline __m128i
-make_odd_epu32(const __m128i a)
+static inline __m128i
+mm_make_odd_epu32(const __m128i a)
 {
     return _mm_or_si128(a, _mm_set1_epi32(1));
 }
 
 /// Make the packed unsigned 64-bit integers odd
-inline __m128i
-make_odd_epu64(const __m128i a)
+static inline __m128i
+mm_make_odd_epu64(const __m128i a)
 {
     return _mm_or_si128(a, _mm_set1_epi64x(1));
 }
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
