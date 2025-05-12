eb9d0c91989ad07a4b12a8ba0d39d89c2f8a6eb6
Steven Ward <planet36@users.noreply.github.com>
2025-05-12 18:26:39 -0400

    Remove fix_resolution.fish

--- other/old/.config/fish/functions/fix_resolution.fish
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Steven Ward
-# SPDX-License-Identifier: OSL-3.0
-
-##### TODO: test this
-function fix_resolution
-    xrandr --output $(xrandr | awk '$2 == "connected"{print $1; exit}') --auto
-end
