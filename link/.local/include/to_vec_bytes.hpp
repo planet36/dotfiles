1fd9c5b03b219118a82f6fd8710857ecad17f50b
Steven Ward <planet36@users.noreply.github.com>
2025-08-04 13:26:51 -0400

    Add function to_vec_uint8

--- link/.local/include/to_vec_bytes.hpp
+++ link/.local/include/to_vec_bytes.hpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Steven Ward
 // SPDX-License-Identifier: OSL-3.0
 
-/// Convert a container to a \c std::vector of \c std::byte
+/// Convert a container to a \c std::vector of \c std::byte or \c uint8_t
 /**
 \file
 \author Steven Ward
@@ -19,4 +19,13 @@ to_vec_bytes(const std::ranges::contiguous_range auto& container)
 {
     const auto sp_bytes = std::as_bytes(std::span(container));
     return sp_bytes | std::ranges::to<std::vector>();
+}
+
+/// Convert a container to a \c std::vector of \c uint8_t
+auto
+to_vec_uint8(const std::ranges::contiguous_range auto& container)
+{
+    auto byte_to_uint8 = [](const std::byte b) { return std::to_integer<uint8_t>(b); };
+    const auto sp_bytes = std::as_bytes(std::span(container));
+    return sp_bytes | std::views::transform(byte_to_uint8) | std::ranges::to<std::vector>();
 }
