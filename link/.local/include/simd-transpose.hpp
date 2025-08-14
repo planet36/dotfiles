1578d0a7a8d4d8ee6b0b66ebdf636af97c372a50
Steven Ward <planet36@users.noreply.github.com>
2025-08-14 16:25:49 -0400

    Add simd-transpose.h, simd-transpose.hpp

---
+++ link/.local/include/simd-transpose.hpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Transpose an array of 128-bit integer vectors (treating it as a square matrix of integers)
+/**
+* \file
+* \author Steven Ward
+*/
+
+#pragma once
+
+#if defined(__x86_64__) && defined(__SSE2__)
+
+#include "simd-transpose.x86.hpp"
+
+#else
+
+#error "Architecture not supported"
+
+#endif
