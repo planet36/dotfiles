bdb59a485307476e49bdca92b061e791d2a4150b
Steven Ward <planet36@users.noreply.github.com>
2025-09-01 11:50:33 -0400

    Double curly braces ("{{") are not needed outside a format string

--- other/generate-src/generate-bit-patterns.py
+++ other/generate-src/generate-bit-patterns.py
@@ -274,7 +274,7 @@ for bits in (16, 32, 64):
         assert i.bit_count() == bits // 2
 
     print(f'namespace bit_patterns_{bits}') # namespace begin
-    print('{{')
+    print('{')
     print()
 
     print(f'inline constexpr std::array<uint{bits}_t, {to_dec_thou_str_cpp(len(bit_patterns[bits]))}> C{{')
