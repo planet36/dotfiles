71a2434417ded9962616b3eb7d8e80c21f66d68b
Steven Ward <planet36@users.noreply.github.com>
2025-10-07 14:48:58 -0400

    Add comment

--- link/.local/include/mum.h
+++ link/.local/include/mum.h
@@ -18,6 +18,7 @@ extern "C" {
 #endif
 
 #if !defined(_mulx_u32)
+// Note: _mulx_u32 is not defined if __x86_64__
 // https://github.com/gcc-mirror/gcc/blob/dfe0d4389a3ce43179563a63046ad3e74d615a08/gcc/config/i386/bmi2intrin.h#L95
 // https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-nonnull-function-attribute
 static inline unsigned int
