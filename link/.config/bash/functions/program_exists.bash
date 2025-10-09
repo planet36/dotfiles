68ed9c876335d7e44f4e6ecefe6584b8edc1caa7
Steven Ward <planet36@users.noreply.github.com>
2025-10-09 19:36:57 -0400

    Remove program_exists.bash

--- link/.config/bash/functions/program_exists.bash
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Steven Ward
-# SPDX-License-Identifier: OSL-3.0
-
-function program_exists
-{
-    # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/command.html
-    command -v "${@}" > /dev/null
-}
