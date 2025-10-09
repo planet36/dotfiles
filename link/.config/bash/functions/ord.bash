632c7963edc9663d17f92f6b9073dbe0aa03362f
Steven Ward <planet36@users.noreply.github.com>
2025-10-09 19:34:10 -0400

    Remove ord.bash

--- link/.config/bash/functions/ord.bash
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Steven Ward
-# SPDX-License-Identifier: OSL-3.0
-
-# https://mywiki.wooledge.org/BashFAQ/071
-# converts ASCII character to its decimal value
-function ord
-{
-    LC_CTYPE=C printf '%d' "'${1}"
-}
