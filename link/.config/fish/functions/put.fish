53e5ab85b5281d3f5f33f04b9d82c53ec62433bd
Steven Ward <planet36@users.noreply.github.com>
2025-10-11 16:41:22 -0400

    Add --mkpath to RSYNC_OPTIONS

--- link/.config/fish/functions/put.fish
+++ link/.config/fish/functions/put.fish
@@ -13,6 +13,7 @@ function put
     # --checksum, -c           skip based on checksum, not mod-time & size
     # --recursive, -r          recurse into directories
     # --update, -u             skip files that are newer on the receiver
+    # --mkpath                 create destination's missing path components
     # --perms, -p              preserve permissions
     # --times, -t              preserve modification times
     # --dry-run, -n            perform a trial run with no changes made
@@ -22,7 +23,7 @@ function put
     # --progress               show progress during transfer
     # --itemize-changes, -i    output a change-summary for all updates
     # --password-file=FILE     read daemon-access password from FILE
-    set RSYNC_OPTIONS -v -c -u -p -t -r -z -h
+    set RSYNC_OPTIONS -v -c -u --mkpath -p -t -r -z -h
 
     for DIR in $argv
         set SRC "$HOME/$DIR/"
