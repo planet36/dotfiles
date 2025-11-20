94ce59e537e4b142353ae4eb68f0d2863a137b71
Steven Ward <planet36@users.noreply.github.com>
2025-11-20 15:09:47 -0500

    Fix warning: use auto when initializing with a cast to avoid duplicating the type name [hicpp-use-auto,modernize-use-auto]

--- link/.local/include/byte_width.hpp
+++ link/.local/include/byte_width.hpp
@@ -19,6 +19,6 @@ byte_width(const std::unsigned_integral auto x)
     // std::bit_width(0) returns 0, but we want it to be 1
     if (x == 0)
         return 1;
-    const unsigned int w = static_cast<unsigned int>(std::bit_width(x));
+    const auto w = static_cast<unsigned int>(std::bit_width(x));
     return (w / 8) + (w % 8 != 0);
 }
