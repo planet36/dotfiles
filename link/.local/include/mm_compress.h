98800b81aa73376dd959cebf1bbf90f58ffd17cc
Steven Ward <planet36@users.noreply.github.com>
2025-12-12 14:04:58 -0500

    Add mm_compress.h

---
+++ link/.local/include/mm_compress.h
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Compress 2 \c __m128i into 1 \c __m128i non-symmetrically and non-linearly
+/**
+* \file
+* \author Steven Ward
+*/
+
+#pragma once
+
+#include <immintrin.h>
+
+#if defined(__AES__)
+static inline __m128i
+mm_compress128(const __m128i a, const __m128i b)
+{
+    return
+        _mm_aesenc_si128(
+                _mm_aesenc_si128(
+                    _mm_aesenc_si128(
+                        _mm_aesenc_si128(b, a),
+                        b),
+                    a),
+                _mm_setzero_si128());
+}
+#endif
