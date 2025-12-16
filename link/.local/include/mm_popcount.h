5981f9769bd0a1aede7d4851609ba0aaa638e79f
Steven Ward <planet36@users.noreply.github.com>
2025-12-16 13:34:29 -0500

    Add function mm256_popcount

--- link/.local/include/mm_popcount.h
+++ link/.local/include/mm_popcount.h
@@ -31,6 +31,19 @@ mm_popcount(const __m128i x)
 }
 #endif
 
+#if defined(__AVX__)
+static inline int
+mm256_popcount(const __m256i x)
+{
+    const uint64_t x0 = (uint64_t)_mm256_extract_epi64(x, 0);
+    const uint64_t x1 = (uint64_t)_mm256_extract_epi64(x, 1);
+    const uint64_t x2 = (uint64_t)_mm256_extract_epi64(x, 2);
+    const uint64_t x3 = (uint64_t)_mm256_extract_epi64(x, 3);
+    return __builtin_popcountg(x0) + __builtin_popcountg(x1) +
+           __builtin_popcountg(x2) + __builtin_popcountg(x3);
+}
+#endif
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
