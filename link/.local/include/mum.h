409de354ce2f3b238ec797524ad7db4378721848
Steven Ward <planet36@users.noreply.github.com>
2026-04-04 08:39:14 -0400

    Suppress warning: function '_mulx_u32' has a definition with different parameter names [readability-inconsistent-declaration-parameter-name]

--- link/.local/include/mum.h
+++ link/.local/include/mum.h
@@ -22,7 +22,7 @@ extern "C" {
 // https://github.com/gcc-mirror/gcc/blob/dfe0d4389a3ce43179563a63046ad3e74d615a08/gcc/config/i386/bmi2intrin.h#L95
 // https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-nonnull-function-attribute
 static inline unsigned int
-_mulx_u32(unsigned int a, unsigned int b, unsigned int* hi) [[gnu::nonnull]]
+_mulx_u32(unsigned int a, unsigned int b, unsigned int* hi) [[gnu::nonnull]] // NOLINT(readability-inconsistent-declaration-parameter-name)
 {
     unsigned long long r = (unsigned long long)a * (unsigned long long)b;
     *hi = (unsigned int)(r >> 32);
