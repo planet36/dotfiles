b8d60751d81e116f9e773c9389e9ed765a06f4a8
Steven Ward <planet36@users.noreply.github.com>
2026-06-04 22:14:09 -0400

    Remove simd-transpose.{arm,x86}.hpp

--- link/.local/include/simd_transpose.hpp
+++ link/.local/include/simd_transpose.hpp
@@ -5,6 +5,14 @@
 /**
 * \file
 * \author Steven Ward
+* \sa https://arm-software.github.io/acle/neon_intrinsics/advsimd.html#zip-elements
+* \sa https://arm-software.github.io/acle/neon_intrinsics/advsimd.html#transpose-elements
+* \sa https://developer.arm.com/architectures/instruction-sets/intrinsics/#f:@navigationhierarchiessimdisa=[Neon]&f:@navigationhierarchiesreturnbasetype=[uint]&q=vzip
+* \sa https://developer.arm.com/architectures/instruction-sets/intrinsics/#f:@navigationhierarchiessimdisa=[Neon]&f:@navigationhierarchiesreturnbasetype=[uint]&q=vtrn
+*
+* TRN1, TRN2, ZIP1, and ZIP2 have the same latency, throughput, and pipelines.
+* https://developer.arm.com/documentation/109824/0002/?lang=en
+* "Arm Cortex-A725 Core Software Optimization Guide"
 */
 
 #pragma once
